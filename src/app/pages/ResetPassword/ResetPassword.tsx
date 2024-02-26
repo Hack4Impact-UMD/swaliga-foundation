c44ec68c3fd709ac77790fd069e437b002216176
Meher Bhamra <mbhamra@terpmail.umd.edu>
2024-02-26 15:51:41 -0500

    final changes (added font, added additional image for logo side, etc.)

--- src/app/pages/ResetPassword/ResetPassword.tsx
+++ src/app/pages/ResetPassword/ResetPassword.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import styles from '../ResetPassword/ResetPassword.module.css';
 import CompanyLogo from '../../images/logo.svg';
+import CompanyLogoWords from '../../images/logo2.svg';
 
 function ResetPasswordPage() {
     return (
@@ -9,11 +10,12 @@ function ResetPasswordPage() {
         <body>
             <div className={styles.page}>
                     <div className={styles.logoSide}>    
-                        <img src={CompanyLogo.src} alt="Company Logo"/>     
+                        <img id={styles.design} src={CompanyLogo.src} alt="Company Logo"/>     
+                        <img id={styles.sitename} src={CompanyLogoWords.src} alt="Company Logo with Words (SwaligaFoundation.org)"/>   
                     </div>
                     <div className={styles.formSide}>
                         <div className={styles.innerBox}>
-                            <h2 className={styles.title}>Reset Password</h2>
+                            <p className={styles.title}>Reset Password</p>
                             <form className={styles.form}>
                                 <p><input className={styles.input} type="text" placeholder="new password"></input></p>
                                 <p><input className={styles.input} type="text" placeholder="confirm password"></input></p>
