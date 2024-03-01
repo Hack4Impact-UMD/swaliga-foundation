9b1a254874a65e6019e64ef560b03d68294dc14b
home0712 <k07m12j@gmail.com>
2024-03-01 12:56:17 -0500

    [TEST] added test buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+"use client"
+import React from 'react';
+import { signInWithGoogle, logOut } from '@/lib/firebase/googleAuth';
+
 export default function Home() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <>
+    <h1>Hello, Next.js!</h1>
+    <div>
+      <button onClick={signInWithGoogle}>Sign in with Google</button> <br></br>
+      <button onClick={logOut}>Sign out</button>
+    </div>
+    </>
+  );
 }
--- src/lib/firebase/googleAuth.tsx
+++ src/lib/firebase/googleAuth.tsx
@@ -2,48 +2,39 @@ import { useEffect } from 'react';
 import { auth } from '../../../firebaseConfig';
 import { signInWithPopup, signOut, GoogleAuthProvider } from 'firebase/auth';
 
-const LoginPage = () => {
-    const signInWithGoogle = async () => {
-        const provider = new GoogleAuthProvider();
-        try {
-            // if the same email id is in the database (not google account)
-            await signInWithPopup(auth, provider)
-            .then((result) => {
-                // the signed-in user info
-                const user = result.user;
+async function signInWithGoogle(): Promise<void> {
+    const provider = new GoogleAuthProvider();
+    try {
+        // if the same email id is in the database (not google account)
+        await signInWithPopup(auth, provider)
+        .then((result) => {
+            // the signed-in user info
+            const user = result.user;
 
-                // get the token from a user -> Promise<string> ??
-                const token = user.getIdToken();
+            // get the token from a user -> Promise<string> ??
+            const token = user.getIdToken();
 
-                // generate Google access Token ??
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const googleToken = credential?.accessToken;
-                console.log('token', token);
-            }).catch((error) => {
-                console.log(error);
-            });
-            // when login succeed
-            // need to add logics
-        } catch (error: unknown) {
-            // const errorCode = error.code;
-            // const errorMessage = error.message;
-            console.error(error);
-            // when an error occurs
-            // need to add logics
-        }
-    };
-
-    const logOut = () => {
+            // generate Google access Token ??
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const googleToken = credential?.accessToken;
+            console.log('token', token);
+        }).catch((error) => {
+            console.log(error);
+        });
+        // when login succeed
         // need to add logics
-        signOut(auth);
-    };
+    } catch (error: unknown) {
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        console.error(error);
+        // when an error occurs
+        // need to add logics
+    }
+};
 
-    return (
-        <div>
-            <button onClick={signInWithGoogle}>Sign in with Google</button> <br></br>
-            <button onClick={logOut}>Sign out</button>
-        </div>
-    ); 
-}
+function logOut(): void {
+    // need to add logics
+    signOut(auth);
+};
 
-export default LoginPage;
+export { signInWithGoogle, logOut };
