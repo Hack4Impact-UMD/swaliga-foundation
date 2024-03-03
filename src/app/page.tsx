68ebef76eed9682b976617f111f02b4cc5a9d213
home0712 <k07m12j@gmail.com>
2024-03-03 15:40:10 -0500

    feat: add the token verification function & update the logout function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { signInWithGoogle, logOut } from '@/lib/firebase/googleAuth';
 export default function Home() {
   return (
     <>
-    <h1>Hello, Next.js!</h1>
+    <h1>Google Login Test</h1>
     <div>
       <button onClick={signInWithGoogle}>Sign in with Google</button> <br></br>
       <button onClick={logOut}>Sign out</button>
--- src/lib/firebase/googleAuth.tsx
+++ src/lib/firebase/googleAuth.tsx
@@ -1,40 +1,55 @@
-import { useEffect } from 'react';
 import { auth } from '../../../firebaseConfig';
+import { FirebaseError } from 'firebase/app';
 import { signInWithPopup, signOut, GoogleAuthProvider } from 'firebase/auth';
+
+async function verifyGoogleToken(googleAccessToken: string | undefined): Promise<boolean> {
+    const response = await fetch(`https://oauth2.googleapis.com/tokeninfo?access_token=${googleAccessToken}`);
+    const data = await response.json();
+
+    if (data.error) { 
+        console.error("Invalid Token", data.error);
+        return false;
+    } else {
+        console.log("Token is valid", data);
+        return true;
+    }
+}
 
 async function signInWithGoogle(): Promise<void> {
     const provider = new GoogleAuthProvider();
     try {
-        // if the same email id is in the database (not google account)
-        await signInWithPopup(auth, provider)
-        .then((result) => {
-            // the signed-in user info
-            const user = result.user;
+        const result = await signInWithPopup(auth, provider);
+        const user = result.user;
 
-            // get the token from a user -> Promise<string> ??
-            const token = user.getIdToken();
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const googleToken = credential?.accessToken; 
 
-            // generate Google access Token ??
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const googleToken = credential?.accessToken;
-            console.log('token', token);
-        }).catch((error) => {
-            console.log(error);
-        });
-        // when login succeed
-        // need to add logics
+        const verified = await verifyGoogleToken(googleToken);
+
+        if (verified) {
+            console.log("Current user verified");
+        } else {
+            console.log("Please sign in again");
+        }
     } catch (error: unknown) {
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        console.error(error);
-        // when an error occurs
-        // need to add logics
-    }
+        if ((error as FirebaseError).code === 'auth/account-exists-with-different-credential') {
+            console.log("Already existing email address");
+        }
+    };
 };
 
 function logOut(): void {
-    // need to add logics
-    signOut(auth);
+    const user = auth.currentUser;
+    if (user) {
+        signOut(auth)
+        .then(() => {
+            console.log("Sign-out successful");
+        }).catch((error) => {
+            console.log(error);
+        });
+    } else {
+        console.log("No user is signed in");
+    }
 };
 
 export { signInWithGoogle, logOut };
