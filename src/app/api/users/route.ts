ad71ba1c2c07f014e98a1d7d71ef728c05b44e14
Meher Bhamra <mbhamra@terpmail.umd.edu>
2024-03-20 17:18:14 -0400

    adjusted code for post request (create a new user account)

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,33 +1,44 @@
 import { createAccount } from "@/lib/firebase/database/users";
-import { NextApiRequest, NextApiResponse } from 'next';
 import { User } from "@/types/User";
+import { NextRequest, NextResponse } from 'next/server';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        try {
-            if (!req.body) {
-                return res.status(400).json({ error: 'missing request body' });
-            }
-
-            const user : User = req.body;
-            
-            if (checkIfValidUser(user)) {
-                try {
-                    const result = await createAccount(user);
-                    return res.status(200).json({ result });
-                } catch {
-                    return res.status(404).json({ error: 'error with creating the account' });
-                }
-            } else {
-                return res.status(400).json({ error: 'invalid user data' });
-            }
-        } catch (err) {
-            res.status(500).json({ error: 'failed to load data' });
+export async function POST(req: NextRequest) {
+    try {
+        if (!req.body) {
+            return NextResponse.json({ error: 'Missing Request Body' }, { status: 400 })
         }
-    } 
+        
+        const data = await req.formData();
+        const user : User = {
+            isAdmin: (String(data.get('isAdmin')) === 'true') || false, 
+            firstName: String(data.get('firstName')) || '', 
+            lastName: String(data.get('lastName')) || '', 
+            address: String(data.get('address')) || '', 
+            school: String(data.get('school')) || '', 
+            birthdate: new Date(String(data.get('birthdate'))) || Date(),
+            email: String(data.get('email')) || '',
+            phone: parseInt(String(data.get('phone'))) || 0, 
+            password: String(data.get('password')) || '', 
+            swaligaId: parseInt(String(data.get('swaligaId'))) || 0, 
+            id: String(data.get('id')) || ''
+        };
+        
+        if (checkIfValidUser(user)) {
+            try {
+                await createAccount(user);
+                return NextResponse.json({ message: 'Account Created Successfully' }, { status: 200 })
+            } catch {
+                return NextResponse.json({ error: 'Error with Creating the Account' }, { status: 404 })
+            }
+        } else {
+            return NextResponse.json({ error: 'Invalid User Data' }, { status: 400 })
+        }
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to Load Data' }, { status: 500 })
+    }
 }
 
 function checkIfValidUser(user : User) {
-    return (user && user.isAdmin && user.firstName && user.lastName && user.address && user.school 
+    return (user && user.firstName && user.lastName && user.address && user.school 
     && user.birthdate && user.email && user.phone && user.password && user.swaligaId && user.id);
 }
