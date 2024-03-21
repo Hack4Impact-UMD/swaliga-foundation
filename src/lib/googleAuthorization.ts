2e178bf02965308e592b2eb8260c2cdabf81fb4a
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-03-20 21:41:55 -0400

    googleAuthorization.ts file

---
+++ src/lib/googleAuthorization.ts
@@ -0,0 +1,20 @@
+import { google } from "googleapis";
+import { redirect } from "next/navigation";
+
+export function authorizeWithGoogle() {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    "http://localhost:3000/__/auth/handler"
+  );
+
+  const scopes = ["https://www.googleapis.com/auth/drive.file"];
+
+  const authorizationUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+    include_granted_scopes: true,
+  });
+
+  redirect(authorizationUrl);
+}
