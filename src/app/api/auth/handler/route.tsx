9db038141cda032d35ac3896d973018acae802f9
home0712 <k07m12j@gmail.com>
2024-03-24 23:07:07 -0400

    revised a test line for auth handler

--- src/app/api/auth/handler/route.tsx
+++ src/app/api/auth/handler/route.tsx
@@ -8,7 +8,7 @@ export async function GET(req: NextRequest) {
     try {
       const { tokens } = await oauth2Client.getToken(query.get("code") as string);
       oauth2Client.setCredentials(tokens);
-      console.log("Test: ", tokens);
+      console.log(tokens);
       return NextResponse.redirect('http://localhost:3000');
     } catch (err) {
       console.error("getting tokens failed");
