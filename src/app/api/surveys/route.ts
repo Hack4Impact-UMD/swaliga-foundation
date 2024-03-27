88dcf8ef5fa89c95242a7523b94d0b4c3cdd9668
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-03-27 14:47:54 -0400

    modified /api/surveys/route.ts to use data from req.body and return a NextResponse

--- src/app/api/surveys/route.ts
+++ src/app/api/surveys/route.ts
@@ -1,53 +1,39 @@
-import { google } from 'googleapis';
-import { NextRequest, NextResponse } from 'next/server';
-import { db } from '@/lib/firebase/firebaseConfig';
-import { collection, addDoc } from "firebase/firestore"; 
+import { NextRequest, NextResponse } from "next/server";
+import { forms } from "@/lib/googleAuthorization";
+import { db } from "@/lib/firebase/firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  // 1-1. get access token from client side
-  const header = req.headers.get("authorization") || "";
-  const accessToken = header.split("Bearer ").at(1);
+export async function POST(req: NextRequest) {
+  const body = await req.json();
 
-  if (!accessToken) {
+  let form = null;
+  try {
+    form = await forms.forms.create({
+      requestBody: {
+        info: {
+          title: body.title,
+          documentTitle: body.documentTitle,
+        },
+      },
+    });
+  } catch (err) {
     return NextResponse.json(
-      { message: "Access token is missing" },
+      { message: "error creating google form" },
       { status: 401 }
     );
   }
 
-  // 1-2. get user credentials (admin)
-  const auth = new google.auth.OAuth2();
-  auth.setCredentials({ access_token: accessToken });
-
-  const forms = google.forms({
-    version: "v1",
-    auth: auth,
-  });
-
-  // 2. create a google form
-  const form = await forms.forms.create({
-    requestBody: {
-      info: {
-        title: "Your Form Title",
-        documentTitle: "Your Document Title",
-      },
-    },
-  });
-
-  console.log("Form Created: ", form.data);
-
-  // 3. store the data into firestore
   try {
-    const formDoc = await addDoc(collection(db, "forms"), {
-      formId: form.data.formId,
-      title: form.data.info?.title,
-      revisionId: form.data.revisionId,
-      responderUri: form.data.responderUri,
-    });
-    console.log("Form added");
+    await addDoc(collection(db, "surveys"), form.data);
+    return NextResponse.json(
+      { message: "survey successfully created" },
+      { status: 200 }
+    );
   } catch (err) {
-    console.log("Error adding document: ", err);
+    console.log(err);
+    return NextResponse.json(
+      { message: "cannot add survey to firestore" },
+      { status: 500 }
+    );
   }
-
-  return NextResponse.json(form.data, { status: 200 });
 }
