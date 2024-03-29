610b6fddb7b75568066a0f7accdf08b789df7bd9
Meher Bhamra <mbhamra@terpmail.umd.edu>
2024-03-28 22:43:20 -0400

    made adjustments according to comments in pr

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -3,54 +3,34 @@ import { User } from "@/types/User";
 import { NextRequest, NextResponse } from 'next/server';
 import { UpdateData } from "firebase/firestore";
 
-export async function GET({ params }: { params: { userId: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { userId: string } }) {
+    const userid = params.userId;
+
     try {
-        const userid = params.userId;
-        
-        if (!userid) {
-            return NextResponse.json({ error: 'Missing UserId' }, { status: 400 });
-        } else {
-            let strUserId = String(userid);
-
-            try {
-                const result = await getAccountById(strUserId);
-                return NextResponse.json({ result }, { status: 200 });
-            } catch {
-                return NextResponse.json({ error: 'Error with Getting the Account Via the Given ID' }, { status: 404 });
-            }
-        }
-
-    } catch (error) {
-        return NextResponse.json({ error: 'Failed to Load Data' }, { status: 500 });
+        const result = await getAccountById(userid);
+        return NextResponse.json({ result }, { status: 200 });
+    } catch {
+        return NextResponse.json({ error: 'Error with Getting the Account Via the Given ID' }, { status: 404 });
     }
 }
 
 export async function PUT(req: NextRequest, { params }: { params: { userId: string }}) {
-    try {
-        if (!req.body) {
-            return NextResponse.json({ error: 'Missing Request Body' }, { status: 400 });
-        }
-
-        const userid = params.userId;
-        const data = await req.formData();
-        let info : UpdateData<User> = Object.fromEntries(data);
-
-        if (!userid) {
-            return NextResponse.json({ error: 'Missing UserID' }, { status: 400 });
-        } else if (!info) {
-            return NextResponse.json({ error: 'Invalid User Data' }, { status: 400 });
-        } else {
-            try {
-                let strUserId = String(userid);
-                await updateAccount(strUserId, info);
-                return NextResponse.json({ message: "Account Successfully Updated" }, { status: 200 });
-            } catch {
-                return NextResponse.json({ error: 'Error with Updating the Account' }, { status: 404 });
-            }
-        }
-        
-    } catch (err) {
-        return NextResponse.json({ error: 'Failed to Load Data' }, { status: 500 });
+    if (!req.body) {
+        return NextResponse.json({ error: 'Missing Request Body' }, { status: 400 });
     }
 
+    const userid = params.userId;
+    const data = await req.json();
+    let info : UpdateData<User> = data;
+
+    if (!info) {
+        return NextResponse.json({ error: 'Invalid User Data' }, { status: 400 });
+    } else {
+        try {
+            await updateAccount(userid, info);
+            return NextResponse.json({ message: "Account Successfully Updated" }, { status: 200 });
+        } catch {
+            return NextResponse.json({ error: 'Error with Updating the Account' }, { status: 404 });
+        }
+    }
 }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -8,22 +8,10 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ error: 'Missing Request Body' }, { status: 400 });
         }
         
-        const data = await req.formData();
-        const user : User = {
-            isAdmin: (String(data.get('isAdmin')) === 'true') || false, 
-            firstName: String(data.get('firstName')) || '', 
-            lastName: String(data.get('lastName')) || '', 
-            address: String(data.get('address')) || '', 
-            school: String(data.get('school')) || '', 
-            birthdate: new Date(String(data.get('birthdate'))) || Date(),
-            email: String(data.get('email')) || '',
-            phone: parseInt(String(data.get('phone'))) || 0, 
-            password: String(data.get('password')) || '', 
-            swaligaId: parseInt(String(data.get('swaligaId'))) || 0, 
-            id: String(data.get('id')) || ''
-        };
+        const data = await req.json();
+        const user : User = data;
         
-        if (checkIfValidUser(user)) {
+        if (user) {
             try {
                 await createAccount(user);
                 return NextResponse.json({ message: 'Account Created Successfully' }, { status: 200 });
@@ -36,9 +24,4 @@ export async function POST(req: NextRequest) {
     } catch (error) {
         return NextResponse.json({ error: 'Failed to Load Data' }, { status: 500 });
     }
-}
-
-function checkIfValidUser(user : User) {
-    return (user && user.firstName && user.lastName && user.address && user.school 
-    && user.birthdate && user.email && user.phone && user.password && user.swaligaId && user.id);
 }
