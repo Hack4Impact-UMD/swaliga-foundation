35a2d932865b11f6e32103c057c833c4a75f898c
Meher Bhamra <mbhamra@terpmail.umd.edu>
2024-03-28 23:31:50 -0400

    removed unneeded if statement

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -11,16 +11,13 @@ export async function POST(req: NextRequest) {
         const data = await req.json();
         const user : User = data;
         
-        if (user) {
-            try {
-                await createAccount(user);
-                return NextResponse.json({ message: 'Account Created Successfully' }, { status: 200 });
-            } catch {
-                return NextResponse.json({ error: 'Error with Creating the Account' }, { status: 404 });
-            }
-        } else {
-            return NextResponse.json({ error: 'Invalid User Data' }, { status: 400 });
+        try {
+            await createAccount(user);
+            return NextResponse.json({ message: 'Account Created Successfully' }, { status: 200 });
+        } catch {
+            return NextResponse.json({ error: 'Error with Creating the Account' }, { status: 404 });
         }
+        
     } catch (error) {
         return NextResponse.json({ error: 'Failed to Load Data' }, { status: 500 });
     }
