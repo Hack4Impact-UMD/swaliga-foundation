a8ab3fa5c43f7056a4eb898971f65279d290aa33
Meher Bhamra <mbhamra@terpmail.umd.edu>
2024-03-30 15:12:18 -0400

    preliminary code for functions creating/renewing watches

---
+++ src/lib/firebase/database/watches.ts
@@ -0,0 +1,22 @@
+import { Watch } from "@/types/watch-types";
+import { forms } from "@/lib/googleAuthorization";
+
+export async function createWatch(formId : string) {
+    try {
+        const response = await forms.forms.watches.create({"formId": formId});
+        console.log("Response", response);
+    } catch (error) {
+        console.log("Error with creating watch using given formId", error);
+        throw error;
+    }
+}
+
+export async function renewWatch(formId : string, watchId : string) {
+    try {
+        const response = await forms.forms.watches.renew({"formId": formId, "watchId": watchId});
+        console.log("Response", response);
+    } catch (error) {
+        console.log("Error with renewing watch using given formId and watchId", error);
+        throw error;
+    }
+}
