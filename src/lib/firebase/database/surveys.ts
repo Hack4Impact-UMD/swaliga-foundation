47d051ca546dbad02459c2b48dd87fec49cf5d58
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-04-08 16:28:38 -0400

    changed code to get survey by document id in firestore

--- src/lib/firebase/database/surveys.ts
+++ src/lib/firebase/database/surveys.ts
@@ -26,33 +26,20 @@ export async function createSurvey(body: {title: string, documentTitle: string})
 }
 
 /* Retrieve form data given form id and update it to firebase.
-* @params id - id of form, not document id of the form.
+* @params id - id of form
 */
-export async function updateForm(id: string) {
+export async function updateSurvey(id: string) {
+  let form = null;
   try {
-    const res = await forms.forms.get({formId: id})
-    console.log("Content to update with: ", res.data);
+    form = await forms.forms.get({formId: id})
+  } catch (err) {
+    throw Error('unable to get google form');
+  }
 
-    /* Find the document with the matching formid */
-    const q = query(collection(db, 'surveys'), where("formId", "==", id));
-    const querySnapshot = await getDocs(q);
-    
-    // Check if there's a matching document
-    if (querySnapshot.size === 0) {
-      console.log("No document with the specified formId found.");
-      return;
-    }
-    
-    //in any case we have multiple form ids, just change the first
-    const docSnapshot = querySnapshot.docs[0];
-    await setDoc(docSnapshot.ref, res.data, { merge: true });
-
-     // Print the form's content to confirm it contains the updated content
-    const formData = docSnapshot.data();
-    console.log("Updated form: ", formData);
-    return formData;
-  } catch (error) {
-    console.error('Error retrieving form or updating form in firebase: ', error);
-    throw error;
+  try {
+    await setDoc(doc(db, 'surveys', form.data.formId || ''), form.data, {merge: true});
+    return form;
+  } catch (err) {
+    throw Error('unable to update survey in firestore');
   }
 }
