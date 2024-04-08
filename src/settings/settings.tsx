eadfb7339af9ab6063a530743ba040665b93ca93
Your Name <jnguyengg@gmail.com>
2024-04-08 11:34:59 -0400

    Removed unnecessary fns and used updateAccount fn

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -16,6 +16,7 @@ import { getAuth } from "firebase/auth";
 import Image from "next/image";
 import Link from "next/link";
 import { logOut } from "@/lib/firebase/authentication/googleAuthentication";
+import { updateAccount } from "@/lib/firebase/database/users";
 
 export default function Settings() {
   const [currUser, setCurrUser] = useState<User>();
@@ -25,8 +26,7 @@ export default function Settings() {
   const [phoneNumber, setPhone] = useState(currUser?.phone);
   const [addr, setAddr] = useState(currUser?.address);
   const [newGradYear, setGradYear] = useState(currUser?.gradYear);
-
-  const q = query(collection(db, "users"), where("id", "==", currUser!.id));
+  const [disabled, isDisabled] = useState(true);
 
   const fetchCurrUser = async (id: string) => {
     const userRef = doc(db, "users", id);
@@ -38,6 +38,8 @@ export default function Settings() {
       console.log("User does not exist");
     }
   };
+  
+  const q = query(collection(db, "users"), where("id", "==", currUser!.id));
 
   useEffect(() => {
     const auth = getAuth();
@@ -53,96 +55,6 @@ export default function Settings() {
     logOut();
   }
 
-  async function handleAddr() {
-    try {
-      const querySnapshot = (await getDocs(q)).docs[0];
-      await updateDoc(querySnapshot.ref, { address: addr });
-
-      console.log("Data with updated address: ", querySnapshot.data);
-    } catch (error) {
-      console.error(
-        "Could not get user or couldn't update user's field",
-        error
-      );
-      throw error;
-    }
-  }
-
-  async function handleFirstName() {
-    try {
-      const querySnapshot = (await getDocs(q)).docs[0];
-      await updateDoc(querySnapshot.ref, { firstName: first });
-
-      console.log("Data with updated first name: ", querySnapshot.data);
-    } catch (error) {
-      console.error(
-        "Could not get user or couldn't update user's field",
-        error
-      );
-      throw error;
-    }
-  }
-
-  async function handleLastName() {
-    try {
-      const querySnapshot = (await getDocs(q)).docs[0];
-      await updateDoc(querySnapshot.ref, { lastName: last });
-
-      console.log("Data with updated last name: ", querySnapshot.data);
-    } catch (error) {
-      console.error(
-        "Could not get user or couldn't update user's field",
-        error
-      );
-      throw error;
-    }
-  }
-
-  async function handleEmail() {
-    try {
-      const querySnapshot = (await getDocs(q)).docs[0];
-      await updateDoc(querySnapshot.ref, { email: newEmail });
-
-      console.log("Data with updated email: ", querySnapshot.data);
-    } catch (error) {
-      console.error(
-        "Could not get user or couldn't update user's field",
-        error
-      );
-      throw error;
-    }
-  }
-
-  async function handlePhone() {
-    try {
-      const querySnapshot = (await getDocs(q)).docs[0];
-      await updateDoc(querySnapshot.ref, { phone: phoneNumber });
-
-      console.log("Data with updated phone number: ", querySnapshot.data);
-    } catch (error) {
-      console.error(
-        "Could not get user or couldn't update user's field",
-        error
-      );
-      throw error;
-    }
-  }
-
-  async function handleGradYear() {
-    try {
-      const querySnapshot = (await getDocs(q)).docs[0];
-      await updateDoc(querySnapshot.ref, { gradYear: newGradYear });
-
-      console.log("Data with updated gradYear: ", querySnapshot.data);
-    } catch (error) {
-      console.error(
-        "Could not get user or couldn't update user's field",
-        error
-      );
-      throw error;
-    }
-  }
-
   function handleCancel() {
     setFirstName(currUser?.firstName);
     setLastName(currUser?.lastName);
@@ -150,6 +62,7 @@ export default function Settings() {
     setPhone(currUser?.phone);
     setGradYear(currUser?.gradYear);
     setAddr(currUser?.address);
+    isDisabled(true);
   }
 
   async function handleSaveChanges() {
@@ -166,6 +79,7 @@ export default function Settings() {
       };
 
       await updateDoc(querySnapshot.ref, userData);
+      await updateAccount(currUser!.id, userData);
 
       fetch(`/api/user/${currUser!.id}`, {
         method: "PUT",
@@ -174,6 +88,7 @@ export default function Settings() {
         },
         body: JSON.stringify(userData),
       });
+      isDisabled(false);
     } catch (error) {
       console.error("Could not get user or couldn't execute PUT", error);
       throw error;
@@ -223,8 +138,9 @@ export default function Settings() {
                 className={styles.inputContainer}
                 defaultValue={currUser?.firstName || "N/A"}
                 onChange={(event) => setFirstName(event.target.value)}
+                disabled={disabled}
               />
-              <button className={styles.editBtn} onClick={handleFirstName}>
+              <button className={styles.editBtn} onClick={()=>isDisabled(false)}>
                 Edit
               </button>
             </div>
@@ -235,8 +151,9 @@ export default function Settings() {
                 className={styles.inputContainer}
                 defaultValue={currUser?.lastName || "N/A"}
                 onChange={(event) => setLastName(event.target.value)}
+                disabled={disabled}
               />
-              <button className={styles.editBtn} onClick={handleLastName}>
+              <button className={styles.editBtn} onClick={()=>isDisabled(false)}>
                 Edit
               </button>
             </div>
@@ -247,8 +164,9 @@ export default function Settings() {
                 className={styles.inputContainer}
                 defaultValue={currUser?.email || "N/A"}
                 onChange={(event) => setEmail(event.target.value)}
+                disabled={disabled}
               />
-              <button className={styles.editBtn} onClick={handleEmail}>
+              <button className={styles.editBtn} onClick={()=>isDisabled(false)}>
                 Edit
               </button>
             </div>
@@ -259,8 +177,9 @@ export default function Settings() {
                 className={styles.inputContainer}
                 defaultValue={currUser?.phone || "N/A"}
                 onChange={(event) => setPhone(parseInt(event.target.value))}
+                disabled={disabled}
               />
-              <button className={styles.editBtn} onClick={handlePhone}>
+              <button className={styles.editBtn} onClick={()=>isDisabled(false)}>
                 Edit
               </button>
             </div>
@@ -271,8 +190,9 @@ export default function Settings() {
                 className={styles.inputContainer}
                 defaultValue={currUser?.address || "N/A"}
                 onChange={(event) => setAddr(event.target.value)}
+                disabled={disabled}
               />
-              <button className={styles.editBtn} onClick={handleAddr}>
+              <button className={styles.editBtn} onClick={()=>isDisabled(false)}>
                 Edit
               </button>
             </div>
@@ -283,8 +203,9 @@ export default function Settings() {
                 className={styles.inputContainer}
                 defaultValue={currUser?.gradYear || "N/A"}
                 onChange={(event) => setGradYear(parseInt(event.target.value))}
+                disabled={disabled}
               />
-              <button className={styles.editBtn} onClick={handleGradYear}>
+              <button className={styles.editBtn} onClick={()=>isDisabled(false)}>
                 Edit
               </button>
             </div>
