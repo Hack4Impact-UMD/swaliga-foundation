401d5a09bfa2e053d7ab618b39623d19e76d79df
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-04-26 01:45:10 -0400

    fixed polygon issues

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -2,148 +2,30 @@
 import React, { useEffect, useState, useCallback } from "react";
 import "konva/lib/shapes/Line";
 import { Stage, Layer, Line } from "react-konva/lib/ReactKonvaCore";
-import { Polygon, Dims } from "@/types/konva-types";
+import { Dims, Polygon } from "@/types/konva-types";
 import styles from "./ForgotPasswordPage.module.css";
+import { getInnerPolygonLayer, getOuterPolygonLayer, getPolygonBorder } from "./polygons";
 
 export default function ForgotPasswordPage() {
-    const [dims, setDims] = useState<Dims>({ width: 0, height: 0 });
-    const [polygons, setPolygons] = useState<Polygon[]>([]);
-    const [polygonOverlay, setPolygonOverlay] = useState<Polygon[]>([]);
+    const [dims, setDims] = useState<Dims>({width: 0, height: 0})
+    const [outerPolygons, setOuterPolygons] = useState<Polygon[]>([]);
+    const [polygonBorder, setPolygonBorder] = useState<Polygon[]>([]);
+    const [innerPolygons, setInnerPolygons] = useState<Polygon[]>([]);
     const [email, setEmail] = useState("");
 
-    const updateDims = useCallback(() => {
-      setDims({ width: window.innerWidth, height: window.innerHeight });
-    }, []);
-
     useEffect(() => {
-      updateDims();
-      window.addEventListener("resize", updateDims);
-      return () => window.removeEventListener("resize", updateDims);
-    }, [updateDims]);
-
-    const generatePolygons = useCallback((dims: Dims) => {
-      const coords: Polygon[] = [
-        {
-          points: [
-            0,
-            0,
-            0,
-
-            dims.height / 30,
-            (50 / 100) * dims.width,
-            dims.height,
-
-            dims.width,
-            dims.height,
-            dims.width,
-            
-            0.45 * dims.height,
-            dims.height / 60,
-            0,
-          ],
-          fill: "#D0D12A",
-        },
-        {
-          points: [
-            0,
-            dims.height / 60,
-            0,
-            dims.height,
-            (50 / 85) * dims.width,
-            dims.height,
-          ],
-          fill: "#295972",
-        },
-        {
-          points: [
-            dims.height / 60,
-            0,
-            dims.width,
-            0,
-            dims.width,
-            0.45 * dims.height,
-          ],
-          fill: "#295972",
-        },
-      ];
-      setPolygons(coords);
+      setAllPolygons(window.innerWidth, window.innerHeight);
+      window.addEventListener("resize", () => {
+        setAllPolygons(window.innerWidth, window.innerHeight);
+      });
     }, []);
 
-    const generatePolygonOverlay = useCallback((dims: Dims) => {
-      const coords: Polygon[] = [
-        {
-          points: [
-            dims.width / 2,
-            0.15 * dims.height,
-            0.875 * dims.width,
-            0.15 * dims.height,
-            0.875 * dims.width,
-            0.39375 * dims.height,
-            dims.width / 2,
-            0.225 * dims.height,
-          ],
-          fill: "#D0D12A",
-        },
-        {
-          points: [
-            dims.width / 2,
-            0.225 * dims.height,
-            0.875 * dims.width,
-            0.39375 * dims.height,
-            0.875 * dims.width,
-            0.85 * dims.height,
-            0.5 * dims.width,
-            0.85 * dims.height,
-          ],
-          fill: "#295972",
-        },
-      ];
-      const borderPolygon1 = {
-        points: [
-          dims.width / 2 + 470, 0.15 * dims.height - 19, // top-left corner
-          dims.width / 2 + 475, 0.15 * dims.height - 19, // top-right corner
-          dims.width / 2 + 475, 0.15 * dims.height + 518, // bottom-right corner
-          dims.width / 2 + 470, 0.15 * dims.height + 514, // bottom-left corner
-        ],
-        fill: 'white',
-      };
-      const borderPolygon2 = {
-        points: [
-          dims.width / 2 - 390, 0.15 * dims.height - 19, // top-left corner
-          dims.width / 2 - 385, 0.15 * dims.height - 19, // top-right corner
-          dims.width / 2 - 385, 0.15 * dims.height + 518, // bottom-right corner
-          dims.width / 2 - 390, 0.15 * dims.height + 514, // bottom-left corner
-        ],
-        fill: 'white',
-      };
-      const borderPolygon3 = {
-        points: [
-          dims.width / 2 - 385, 0.15 * dims.height + 513, // bottom-right corner
-          dims.width / 2 - 390, 0.15 * dims.height + 517, // bottom-left corner
-          dims.width / 2 + 475, 0.15 * dims.height + 517, // bottom-right corner
-          dims.width / 2 + 470, 0.15 * dims.height + 513, // bottom-left corner
-        ],
-        fill: 'white',
-      };
-      const borderPolygon4 = {
-        points: [
-          dims.width / 2 - 385, 0.15 * dims.height - 19, // bottom-right corner
-          dims.width / 2 - 390, 0.15 * dims.height - 15, // bottom-left corner
-          dims.width / 2 + 475, 0.15 * dims.height - 15, // bottom-right corner
-          dims.width / 2 + 470, 0.15 * dims.height - 19, // bottom-left corner
-        ],
-        fill: 'white',
-      };
-        const updatedCoords = [...coords, borderPolygon1, borderPolygon2, borderPolygon3, borderPolygon4];
-  setPolygonOverlay(updatedCoords);
-    }, []);
-
-    useEffect(() => {
-      if (dims.width && dims.height) {
-        generatePolygons(dims);
-        generatePolygonOverlay(dims);
-      }
-    }, [dims, generatePolygons]);
+    const setAllPolygons = (width: number, height: number): void => {
+      setDims({ width, height });
+      setOuterPolygons(getOuterPolygonLayer(window.innerWidth, window.innerHeight));
+      setPolygonBorder(getPolygonBorder(window.innerWidth, window.innerHeight));
+      setInnerPolygons(getInnerPolygonLayer(window.innerWidth, window.innerHeight));
+    }
 
     const drawPolygon = (polygon: Polygon) => (
       <Line
@@ -161,8 +43,9 @@ export default function ForgotPasswordPage() {
             <div className={styles.background}>
                 <Stage className={styles.stage} width={dims.width} height={dims.height}>
                     <Layer>
-                        {polygons.map(drawPolygon)}
-                        {polygonOverlay.map(drawPolygon)}
+                        {outerPolygons.map(drawPolygon)}
+                        {polygonBorder.map(drawPolygon)}
+                        {innerPolygons.map(drawPolygon)}
                     </Layer>
                 </Stage>
                 <div className={styles.passwordReset}>
