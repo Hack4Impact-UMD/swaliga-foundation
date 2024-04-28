68aa6f0687bc5dc63d37b44012b5b45e2bd276a1
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-04-28 14:08:50 -0400

    added RequireSignedOut check to LoginPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import GoogleButton from "react-google-button";
 import { loginUser } from "@/lib/firebase/authentication/emailPasswordAuthentication";
 import { signInWithGoogle } from "@/lib/firebase/authentication/googleAuthentication";
+import RequireSignedOut from "@/components/auth/RequireSignedOut";
 
 export default function LoginPage() {
   const [dims, setDims] = useState<Dims>({width: 0, height: 0});
@@ -50,58 +51,66 @@ export default function LoginPage() {
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.background}>
-        <Stage className={styles.stage} width={dims.width} height={dims.height}>
-          <Layer>
-            {polygonBackground.map(drawPolygon)}
-            {polygonOverlay.map(drawPolygon)}
-          </Layer>
-        </Stage>
-        <div className={styles.login}>
-          <h1 className={styles.login_title}>Login</h1>
-          <div className={styles.login_field_container}>
-            <input
-              type="email"
-              className={styles.login_field}
-              placeholder="enter email"
-              value={email}
-              onChange={(ev) => setEmail(ev.target.value)}
+    <RequireSignedOut>
+      <div className={styles.container}>
+        <div className={styles.background}>
+          <Stage
+            className={styles.stage}
+            width={dims.width}
+            height={dims.height}
+          >
+            <Layer>
+              {polygonBackground.map(drawPolygon)}
+              {polygonOverlay.map(drawPolygon)}
+            </Layer>
+          </Stage>
+          <div className={styles.login}>
+            <h1 className={styles.login_title}>Login</h1>
+            <div className={styles.login_field_container}>
+              <input
+                type="email"
+                className={styles.login_field}
+                placeholder="enter email"
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
+              />
+            </div>
+            <div className={styles.login_field_container}>
+              <input
+                type={passwordVisibility ? "text" : "password"}
+                className={styles.login_field}
+                placeholder="password"
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+              />
+              <i
+                className={
+                  styles.icon +
+                  ` fas ${passwordVisibility ? "fa-eye" : "fa-eye-slash"}`
+                }
+                onClick={() => setPasswordVisibility(!passwordVisibility)}
+              />
+            </div>
+            <div className={styles.forgot_password}>
+              <a href="/forgot-password">Forgot password?</a>
+            </div>
+            <button className={styles.login_button} onClick={signInWithEmail}>
+              Submit
+            </button>
+            <p>
+              Click <a href="/create-account">here</a> to create an account
+            </p>
+            <div className={styles.google_button}>
+              <GoogleButton onClick={signInWithGoogle} />
+            </div>
+            <img
+              className={styles.logo}
+              src="/swaliga-website-logo.png"
+              alt="swaliga-logo"
             />
           </div>
-          <div className={styles.login_field_container}>
-            <input
-              type={passwordVisibility ? "text" : "password"}
-              className={styles.login_field}
-              placeholder="password"
-              value={password}
-              onChange={(ev) => setPassword(ev.target.value)}
-            />
-            <i
-              className={
-                styles.icon +
-                ` fas ${passwordVisibility ? "fa-eye" : "fa-eye-slash"}`
-              }
-              onClick={() => setPasswordVisibility(!passwordVisibility)}
-            />
-          </div>
-          <div className={styles.forgot_password}>
-            <a href="/forgot-password">Forgot password?</a>
-          </div>
-          <button className={styles.login_button} onClick={signInWithEmail}>Submit</button>
-          <p>
-            Click <a href="/create-account">here</a> to create an account
-          </p>
-          <div className={styles.google_button}>
-            <GoogleButton onClick={signInWithGoogle}/>
-          </div>
-          <img
-            className={styles.logo}
-            src="/swaliga-website-logo.png"
-            alt="swaliga-logo"
-          />
         </div>
       </div>
-    </div>
+    </RequireSignedOut>
   );
 }
