92c9d2e3ca528c134a50aa7ade7172d3c1c1c997
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-06-20 15:10:40 -0400

    added Filter component to UserList and cleaned up UI

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -2,8 +2,11 @@
 
 import React, { useState } from 'react';
 import styles from './Filter.module.css';
+import { FaTimes } from 'react-icons/fa';
 
-export default function Filter() {
+ export default function Filter(props: { onClose: () => void }) {
+    const { onClose } = props;
+
     // Checkbox states
     const [lastNameChecked, setLastNameChecked] = useState(false);
     const [gradYearChecked, setGradYearChecked] = useState(false);
@@ -18,75 +21,73 @@ export default function Filter() {
 
     // Return the component
     return (
-        <div className={styles.container}>
-            <div className={styles.inputGroup}>
-                <input
-                    type="checkbox"
-                    id="lastNameCheckbox"
-                    className={styles.inputCheckbox}
-                    checked={lastNameChecked}
-                    onChange={(ev) => setLastNameChecked(ev.target.checked)}
-                />
-                <input
-                    name="lastName"
-                    className={styles.inputField}
-                    placeholder="Last Name"
-                    value={lastName}
-                    onChange={(ev) => setLastName(ev.target.value)}
-                />
-            </div>
-            <div className={styles.inputGroup}>
-                <input
-                    type="checkbox"
-                    id="gradYearCheckbox"
-                    className={styles.inputCheckbox}
-                    checked={gradYearChecked}
-                    onChange={(ev) => setGradYearChecked(ev.target.checked)}
-                />
-                <input
-                    name="gradYear"
-                    className={styles.inputField}
-                    placeholder="Grad Year"
-                    value={gradYear}
-                    onChange={(ev) => setGradYear(ev.target.value)}
-                />
-            </div>
-            <div className={styles.inputGroup}>
-                <input
-                    type="checkbox"
-                    id="genderCheckbox"
-                    className={styles.inputCheckbox}
-                    checked={genderChecked}
-                    onChange={(ev) => setGenderChecked(ev.target.checked)}
-                />
-                <input
-                    name="gender"
-                    className={styles.inputField}
-                    placeholder="Gender"
-                    value={gender}
-                    onChange={(ev) => setGender(ev.target.value)}
-                />
-            </div>
-            <div className={styles.inputGroup}>
-                <input
-                    type="checkbox"
-                    id="ethnicityCheckbox"
-                    className={styles.inputCheckbox}
-                    checked={ethnicityChecked}
-                    onChange={(ev) => setEthnicityChecked(ev.target.checked)}
-                />
-                <input
-                    name="ethnicity"
-                    className={styles.inputField}
-                    placeholder="Ethnicity"
-                    value={ethnicity}
-                    onChange={(ev) => setEthnicity(ev.target.value)}
-                />
-            </div>
-
-            <button className={styles.button}> APPLY </button> 
-            <span className={styles.closeIcon}></span>
-            <span className={styles.filterIcon}></span>
+      <div className={styles.container}>
+        <div className={styles.closeIcon} onClick={onClose} />
+        <div className={styles.inputGroup}>
+          <input
+            type="checkbox"
+            id="lastNameCheckbox"
+            className={styles.inputCheckbox}
+            checked={lastNameChecked}
+            onChange={(ev) => setLastNameChecked(ev.target.checked)}
+          />
+          <input
+            name="lastName"
+            className={styles.inputField}
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(ev) => setLastName(ev.target.value)}
+          />
         </div>
+        <div className={styles.inputGroup}>
+          <input
+            type="checkbox"
+            id="gradYearCheckbox"
+            className={styles.inputCheckbox}
+            checked={gradYearChecked}
+            onChange={(ev) => setGradYearChecked(ev.target.checked)}
+          />
+          <input
+            name="gradYear"
+            className={styles.inputField}
+            placeholder="Grad Year"
+            value={gradYear}
+            onChange={(ev) => setGradYear(ev.target.value)}
+          />
+        </div>
+        <div className={styles.inputGroup}>
+          <input
+            type="checkbox"
+            id="genderCheckbox"
+            className={styles.inputCheckbox}
+            checked={genderChecked}
+            onChange={(ev) => setGenderChecked(ev.target.checked)}
+          />
+          <input
+            name="gender"
+            className={styles.inputField}
+            placeholder="Gender"
+            value={gender}
+            onChange={(ev) => setGender(ev.target.value)}
+          />
+        </div>
+        <div className={styles.inputGroup}>
+          <input
+            type="checkbox"
+            id="ethnicityCheckbox"
+            className={styles.inputCheckbox}
+            checked={ethnicityChecked}
+            onChange={(ev) => setEthnicityChecked(ev.target.checked)}
+          />
+          <input
+            name="ethnicity"
+            className={styles.inputField}
+            placeholder="Ethnicity"
+            value={ethnicity}
+            onChange={(ev) => setEthnicity(ev.target.value)}
+          />
+        </div>
+        <button className={styles.button}> APPLY </button>
+      </div>
     );
 }
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -5,38 +5,32 @@ import { User } from "@/types/user-types";
 import styles from "./UserList.module.css";
 import { FaFilter, FaSearch, FaTimes } from "react-icons/fa";
 import { useState } from "react";
+import Filter from "./Filter";
 
 export default function UserList(props: { users: User[]; surveys: Survey[] }) {
   const { users, surveys } = props;
   const [isAllSelected, setIsAllSelected] = useState<boolean>(false);
   const [selectedStudentIds, setSelectedStudentIds] = useState<string[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(0);
+  const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
 
   const studentsPerPage = 50;
   const totalPages = Math.ceil(users.length / studentsPerPage);
 
   const handleStudentCheck = (id: string): void => {
     if (selectedStudentIds.includes(id)) {
-      setSelectedStudentIds(selectedStudentIds.filter((studentId) => studentId !== id));
+      setSelectedStudentIds(
+        selectedStudentIds.filter((studentId) => studentId !== id)
+      );
     } else {
       setSelectedStudentIds([...selectedStudentIds, id]);
     }
-  }
+  };
+
+  const toggleFilter = () => setIsFilterOpen(!isFilterOpen);
 
   return (
     <>
-      <div className={styles.controls}>
-        <div className={styles.searchBox}>
-          <FaSearch className={styles.searchIcon} />
-          <input type="text" placeholder="Search" />
-          <FaTimes className={styles.closeIcon} />
-        </div>
-        <div className={styles.filterBox}>
-          <FaFilter className={styles.filterIcon} />
-          <button>Filter</button>
-          <FaTimes className={styles.closeIcon} />
-        </div>
-      </div>
       <div className={styles.content}>
         <div className={styles.tableContainer}>
           <table className={styles.table}>
@@ -58,44 +52,66 @@ export default function UserList(props: { users: User[]; surveys: Survey[] }) {
               </tr>
             </thead>
             <tbody>
-              {users.slice(currentPage * studentsPerPage, (currentPage + 1) * studentsPerPage).map((student) => (
-                <tr
-                  key={student.id}
-                  className={student.id in selectedStudentIds ? styles.checkedRow : ""}
-                >
-                  <td>
-                    <input
-                      type="checkbox"
-                      checked={selectedStudentIds.includes(student.id)}
-                      onChange={() => handleStudentCheck(student.id)}
-                    />
-                  </td>
-                  <td>{student.middleName ? `${student.firstName} ${student.middleName} ${student.lastName}` : `${student.firstName} ${student.lastName}`}</td>
-                  <td>{student.id}</td>
-                  <td>{student.birthdate?.toUTCString()}</td>
-                  <td>{student.address.city}</td>
-                  <td>{student.email}</td>
-                </tr>
-              ))}
+              {users
+                .slice(
+                  currentPage * studentsPerPage,
+                  (currentPage + 1) * studentsPerPage
+                )
+                .map((student) => (
+                  <tr
+                    key={student.id}
+                    className={
+                      student.id in selectedStudentIds ? styles.checkedRow : ""
+                    }
+                  >
+                    <td>
+                      <input
+                        type="checkbox"
+                        checked={selectedStudentIds.includes(student.id)}
+                        onChange={() => handleStudentCheck(student.id)}
+                      />
+                    </td>
+                    <td>
+                      {student.middleName
+                        ? `${student.firstName} ${student.middleName} ${student.lastName}`
+                        : `${student.firstName} ${student.lastName}`}
+                    </td>
+                    <td>{student.id}</td>
+                    <td>{student.birthdate?.toUTCString()}</td>
+                    <td>{student.address?.city}</td>
+                    <td>{student.email}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
-        <div className={styles.pagination}>
-          <button
-            className={styles.paginationButton}
-            onClick={() => setCurrentPage(Math.max(0, currentPage - 1))}
-            disabled={currentPage === 0}
-          >
-            Previous 50 Students
-          </button>
-          <button
-            className={styles.paginationButton}
-            onClick={() => setCurrentPage(Math.min(totalPages - 1, currentPage + 1))}
-            disabled={currentPage >= totalPages - 1}
-          >
-            Next 50 Students
-          </button>
+        <div className={styles.filterContainer}>
+          {isFilterOpen ? (
+            <Filter onClose={toggleFilter}/>
+          ) : (
+            <div className={styles.filterBox} onClick={toggleFilter}>
+              <FaFilter className={styles.filterIcon}/>
+            </div>
+          )}
         </div>
+      </div>
+      <div className={styles.pagination}>
+        <button
+          className={styles.paginationButton}
+          onClick={() => setCurrentPage(Math.max(0, currentPage - 1))}
+          disabled={currentPage === 0}
+        >
+          Previous 50 Students
+        </button>
+        <button
+          className={styles.paginationButton}
+          onClick={() =>
+            setCurrentPage(Math.min(totalPages - 1, currentPage + 1))
+          }
+          disabled={currentPage >= totalPages - 1}
+        >
+          Next 50 Students
+        </button>
       </div>
     </>
   );
