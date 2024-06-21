c13ccdad13094aa1ab8560f9b62fc68f2cdad01a
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-06-20 23:04:57 -0400

    added more fields to the Filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -7,15 +7,36 @@ import { User } from '@/types/user-types';
  export default function Filter(props: { users: User[], closeFilter: () => void }) {
     const { users, closeFilter } = props;
 
+    const [id, setId] = useState<number | undefined>(undefined);
+    const [firstName, setFirstName] = useState<string>("");
     const [lastName, setLastName] = useState<string>("");
-    const [gradYear, setGradYear] = useState<string>("");
+    const [gradYear, setGradYear] = useState<number | undefined>(undefined);
+    const [age, setAge] = useState<number | undefined>(undefined);
     const [gender, setGender] = useState<string>("");
     const [ethnicity, setEthnicity] = useState<string>("");
+    const [city, setCity] = useState<string>("");
+    
 
     // Return the component
     return (
       <div className={styles.container}>
         <div className={styles.closeIcon} onClick={closeFilter} />
+        <input
+          name="id"
+          className={styles.inputField}
+          type="number"
+          placeholder="ID"
+          value={id}
+          onChange={(ev) => setId(parseInt(ev.target.value))}
+        />
+        <input
+          name="firstName"
+          className={styles.inputField}
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(ev) => setFirstName(ev.target.value)}
+        />
         <input
           name="lastName"
           className={styles.inputField}
@@ -30,7 +51,15 @@ import { User } from '@/types/user-types';
           type="number"
           placeholder="Grad Year"
           value={gradYear}
-          onChange={(ev) => setGradYear(ev.target.value)}
+          onChange={(ev) => setGradYear(parseInt(ev.target.value))}
+        />
+        <input
+          name="age"
+          className={styles.inputField}
+          type="number"
+          placeholder="Age"
+          value={age}
+          onChange={(ev) => setAge(parseInt(ev.target.value))}
         />
         <input
           name="gender"
@@ -45,6 +74,14 @@ import { User } from '@/types/user-types';
           placeholder="Ethnicity"
           value={ethnicity}
           onChange={(ev) => setEthnicity(ev.target.value)}
+        />
+        <input
+          name="city"
+          className={styles.inputField}
+          type="text"
+          placeholder="City"
+          value={city}
+          onChange={(ev) => setCity(ev.target.value)}
         />
         <button className={styles.button}> APPLY </button>
       </div>
