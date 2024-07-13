be6f04caaf597430a502e8332756e3461d89e5d9
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-07-13 19:37:44 -0400

    changed Firebase Admin SDK credentials from default to the service account credentials

--- src/lib/firebase/firebaseAdminConfig.ts
+++ src/lib/firebase/firebaseAdminConfig.ts
@@ -7,7 +7,9 @@ const app =
   existingApps.length === 0
     ? initializeApp(
         {
-          credential: credential.applicationDefault(),
+          credential: credential.cert(
+            process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CREDENTIALS_FILEPATH as string
+          ),
           projectId: "swaliga-foundation",
           databaseURL: "https://swaliga-foundation-default-rtdb.firebaseio.com",
         },
