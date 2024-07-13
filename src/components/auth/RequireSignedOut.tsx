ab9f3704aab734f47d7431dc82ea00007dc7f878
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-07-13 19:36:30 -0400

    added a check for a logged in user to RequireSignedOut

--- src/components/auth/RequireSignedOut.tsx
+++ src/components/auth/RequireSignedOut.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 import AuthProvider, { useAuth } from "./AuthProvider";
+import { auth } from "@/lib/firebase/firebaseConfig";
 
 export default function RequireSignedOut({
   children,
@@ -9,7 +10,7 @@ export default function RequireSignedOut({
   const authContext = useAuth();
   if (authContext.loading) {
     return <p>Loading</p>;
-  } else if (authContext.user) {
+  } else if (authContext.user && authContext.token?.claims?.role) {
     redirect(authContext.token?.claims?.role == "ADMIN" ? '/admin-dashboard' : '/student-dashboard');
   }
 
