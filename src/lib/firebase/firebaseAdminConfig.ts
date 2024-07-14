9bc6c081e1f116680cd3523a87a1a0a261737f57
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-07-13 20:29:08 -0400

    moved the creation of adminApp into a function

--- src/lib/firebase/firebaseAdminConfig.ts
+++ src/lib/firebase/firebaseAdminConfig.ts
@@ -2,19 +2,24 @@ import { credential } from "firebase-admin";
 import { getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const existingApps = getApps();
-const app =
-  existingApps.length === 0
-    ? initializeApp(
-        {
-          credential: credential.cert(
-            process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CREDENTIALS_FILEPATH as string
-          ),
-          projectId: "swaliga-foundation",
-          databaseURL: "https://swaliga-foundation-default-rtdb.firebaseio.com",
-        },
-        "adminApp"
-      )
-    : existingApps[0];
+function getAdminApp() {
+  const existingApps = getApps();
+  const app =
+    existingApps.length === 0
+      ? initializeApp(
+          {
+            credential: credential.cert(
+              process.env
+                .NEXT_PUBLIC_FIREBASE_ADMIN_CREDENTIALS_FILEPATH as string
+            ),
+            projectId: "swaliga-foundation",
+            databaseURL: "https://swaliga-foundation-default-rtdb.firebaseio.com",
+          },
+          "adminApp"
+        )
+      : existingApps[0];
 
-export const adminAuth = getAuth(app);
+  return app;
+}
+
+export const adminAuth = getAuth(getAdminApp());
