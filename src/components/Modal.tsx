8c0331dda478d4d823643bd84ae3857a6ade01a4
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-07-17 21:32:27 -0400

    made the Modal component look better and added close button

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,18 @@
 import styles from './Modal.module.css';
 
-export default function Modal(props: { children: JSX.Element }) {
-    return (
-      <div className={styles.modalBackground}>
-        <dialog className={styles.modal} open>
-          {props.children}
-        </dialog>
-      </div>
-    );
+interface ModalProps {
+  children: JSX.Element;
+  closeModal: () => void;
+}
+
+export default function Modal(props: ModalProps) {
+  const { children, closeModal } = props;
+  return (
+    <div className={styles.modalBackground}>
+      <dialog className={styles.modal} open>
+        <span className={styles.closeIcon} onClick={closeModal} />
+        {children}
+      </dialog>
+    </div>
+  );
 }
