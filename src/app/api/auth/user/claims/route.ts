909b6c2b2421a08f30e62ae688a0949ab59b195f
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-08-10 19:08:40 -0400

    removed unnecessary console.log()

--- src/app/api/auth/user/claims/route.ts
+++ src/app/api/auth/user/claims/route.ts
@@ -4,7 +4,6 @@ import { adminAuth } from "@/lib/firebase/firebaseAdminConfig";
 // sets the role user claim to "STUDENT" for new account with the given uid
 export async function POST(req: NextRequest): Promise<NextResponse> {
   const body = await req.json();
-  console.log(body);
   try {
     await adminAuth.setCustomUserClaims(body.uid, { role: "STUDENT" });
     return NextResponse.json({ success: true }, { status: 200 });
