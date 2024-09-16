1f90e75e547d1b06916e167a95feaecdcc8e6bb3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-09-16 14:49:03 -0400

    removed unnecessary creation of user object in Firestore from emailPasswordAuthentication.ts

--- src/lib/firebase/authentication/emailPasswordAuthentication.ts
+++ src/lib/firebase/authentication/emailPasswordAuthentication.ts
@@ -32,35 +32,6 @@ export const signUpUser = async (
       throw new Error("Failed to sign up user: UID is null or undefined.");
     }
 
-    // Create the user document in Firestore with default values
-    const userDoc = {
-      isAdmin: false,
-      firstName: "",  // Default values, update later
-      middleName: "", // Default values, update later
-      lastName: "",   // Default values, update later
-      email: user.email,
-      phone: 0,       // Default values, update later
-      gender: "",     // Default values, update later
-      birthdate: null, // Default values, update later
-      guardian: [],   // Default values, update later
-      id: userUid,
-      address: {
-        street: "",
-        city: "",
-        state: "",
-        zip: 0,
-        country: "",
-      },
-      school: "",
-      gradYear: 0,
-      yearsWithSwaliga: 0,
-      swaligaID: 0,
-      ethnicity: [], 
-      assignedSurveys: [],
-      completedResponses: [],
-    };
-
-    await setDoc(doc(db, 'users', userUid), userDoc);
     await fetch("/api/auth/claims/registering", {
       method: "POST",
       headers: {
