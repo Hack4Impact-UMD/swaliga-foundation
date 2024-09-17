1b8b24f7c0af0fa884f0b9c0aa6e55063865b697
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-09-17 01:11:54 -0400

    upped renewing of watches to 3 times a week in case of server outages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,7 +15,7 @@ exports.handleFormWatch = onMessagePublished("projects/swaliga-foundation/topics
   })
 });
 
-// "0 0 * * 0,4" - every Sunday & Thursday at midnight, renew all watches of currently active forms
+// "0 0 * * 0,3,5" - every Sunday, Wednesday, and Friday at midnight, renew all watches of currently active forms
 exports.setWatchRenewal = onSchedule("0 0 * * 0,4", async (_) => {
   await fetch(`${URL_PREFIX}/api/watches/renewAll`, {
     method: "POST",
