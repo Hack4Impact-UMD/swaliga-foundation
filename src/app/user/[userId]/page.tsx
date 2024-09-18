d2e3e220ed72bad5fa7f600a643766f520b08a5d
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-09-18 14:35:02 -0400

    deleted generateStaticParams function

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -1,11 +1,4 @@
-import { User } from "@/types/user-types";
 import StudentInfoPage from "./StudentInfoPage";
-import { getAllUsers } from "@/lib/firebase/database/users";
-
-export async function generateStaticParams() {
-    const users = await getAllUsers();
-    return users.map((user: User) => ({ userId: user.id || 'bruh' }));
-}
 
 export default function StudentInfoPageWrapper({ params }: { params: { userId: string }}) {
     return <StudentInfoPage params={params} />
