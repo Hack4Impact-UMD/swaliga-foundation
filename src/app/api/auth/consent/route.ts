f499b8f190dd065f6ec6a9c12df6cb9ebcbe7ac1
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-10-13 16:57:51 -0400

    logger changes

--- src/app/api/auth/consent/route.ts
+++ src/app/api/auth/consent/route.ts
@@ -6,10 +6,13 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const queryParams = req.nextUrl.searchParams;
   if (!queryParams.get("idToken")) {
+    await fetch(`https://logger-fuicsqotja-uk.a.run.app?message=${`idToken not found`}`);
     return NextResponse.redirect("https://swaliga-foundation.web.app");
   } else if (!(await isUserAdmin(queryParams.get("idToken") || ""))) {
+    await fetch(`https://logger-fuicsqotja-uk.a.run.app?message=${`user doesn't have admin privileges`}`);
     return NextResponse.redirect("https://swaliga-foundation.web.app");
   }
   const authUrl = await getAuthUrl();
+  await fetch(`https://logger-fuicsqotja-uk.a.run.app?message=${authUrl}`);
   redirect(authUrl);
 }
