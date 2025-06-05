1ec045c52090211aa460f0287b53e04d9b01c533
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-04 23:55:58 -0400

    streamlined OAuth 2.0 authorization flow & adapted for Google Apps Script

--- src/app/api/auth/consent/route.ts
+++ src/app/api/auth/consent/route.ts
@@ -1,29 +1,9 @@
-import { isUserAdmin } from "@/features/auth/serverAuthentication";
-import { getAuthUrl } from "@/features/auth/googleAuthorization";
-import { redirect } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const queryParams = req.nextUrl.searchParams;
-  if (!queryParams.get("idToken")) {
-    await fetch(`https://logger-fuicsqotja-uk.a.run.app?message=${`idToken not found`}`);
-    console.log('failure 1')
-    return NextResponse.redirect("https://swaliga-foundation.web.app");
-  } else if (!(await isUserAdmin(queryParams.get("idToken") || ""))) {
-    console.log('failure 2')
-    await fetch(`https://logger-fuicsqotja-uk.a.run.app?message=${`user doesn't have admin privileges`}`);
-    return NextResponse.redirect("https://swaliga-foundation.web.app");
-  }
-  const authUrl = await getAuthUrl();
-  console.log(authUrl);
-  await fetch(`https://logger-fuicsqotja-uk.a.run.app?message=${authUrl}`);
-  try {
-    console.log('success')
-    return NextResponse.redirect(authUrl);
-  } catch (e: any) {
-    console.log('failure 3')
-    console.log(e)
-    await fetch(`https://logger-fuicsqotja-uk.a.run.app?message=${`error: ${e}`}`);
-    return NextResponse.redirect("https://swaliga-foundation.web.app");
-  }
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const redirectUri = encodeURIComponent('http://localhost:3000/api/auth/handler');
+  const scope = encodeURIComponent(['https://www.googleapis.com/auth/script.external_request', 'https://www.googleapis.com/auth/script.scriptapp'].join(' '));
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
+  return NextResponse.redirect(authUrl);
 }
--- src/app/api/auth/handler/route.ts
+++ src/app/api/auth/handler/route.ts
@@ -1,20 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import { redirect } from "next/navigation";
-import { isRefreshTokenValid, setAdminRefreshToken } from "@/features/auth/googleAuthorization";
 
 export async function GET(req: NextRequest) {
-  const query = req.nextUrl.searchParams;
-  if (query.get("code")) {
-    const authCode = query.get("code") || "";
-    const updateNeeded = !(await isRefreshTokenValid());
-    if (updateNeeded) {
-      const credsSet = await setAdminRefreshToken(authCode);
-      if (credsSet) {
-        return NextResponse.redirect("https://swaliga-foundation.web.app");
-      }
-    }
-    redirect("/");
-  }
-  console.error(query.get("error"));
-  redirect("/");
+  const code = req.nextUrl.searchParams.get("code");
+  const response = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      code: code || "",
+      client_id: process.env.GOOGLE_CLIENT_ID || "",
+      client_secret: process.env.GOOGLE_CLIENT_SECRET || "",
+      redirect_uri: 'http://localhost:3000/api/auth/handler',
+      grant_type: 'authorization_code',
+    }),
+  });
+  const tokenData = await response.json();
+  return NextResponse.json(tokenData, { status: 200 });
 }
