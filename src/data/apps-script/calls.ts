aa05b0b81e851b89e1c7e94f13c7561f25c89d4c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-07 14:32:48 -0400

    created callAppsScript function

---
+++ src/data/apps-script/calls.ts
@@ -0,0 +1,22 @@
+export async function callAppsScript(accessToken: string, functionName: string, parameters?: any[]): Promise<any> {
+  const response = await fetch(
+    `https://script.googleapis.com/v1/scripts/${process.env.GOOGLE_APPS_SCRIPT_DEPLOYMENT_ID}:run`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        function: functionName,
+        parameters: parameters,
+      }),
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+    }
+  );
+  const data = await response.json();
+  if (data.error) {
+    throw new Error("An unexpected error occurred. Please try again later.");
+  }
+  return data.response.result;
+}
