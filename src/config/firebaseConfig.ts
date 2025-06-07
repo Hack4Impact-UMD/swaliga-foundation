e0f44a536e34c05b6969f64af68d0c6c8aef3e0a
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-07 18:38:31 -0400

    separated dev & prod environments

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -18,6 +18,8 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app, "us-central1");
 
-connectAuthEmulator(auth, 'http://localhost:9099');
-connectFirestoreEmulator(db, 'localhost', 8080);
-connectFunctionsEmulator(functions, "localhost", 5001);
+if (process.env.NODE_ENV === "development") {
+  connectAuthEmulator(auth, 'http://localhost:9099');
+  connectFirestoreEmulator(db, 'localhost', 8080);
+  connectFunctionsEmulator(functions, "localhost", 5001);
+}
