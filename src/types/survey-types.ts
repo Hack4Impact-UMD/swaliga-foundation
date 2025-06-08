81b72d828ddcd695637b9c0814da7cb00dd43572
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-08 15:03:26 -0400

    updated survey types to work with Firestore

--- src/types/survey-types.ts
+++ src/types/survey-types.ts
@@ -1,17 +1,20 @@
+import { ID } from "./utils";
+
 export interface Survey {
-  id: string;
   name: string;
   responderUri: string;
   linkedSheetId?: string;
 }
+export interface SurveyID extends Survey, ID { }
 
 export interface SurveyAssignment {
-  id: string;
   surveyId: string;
   assignedAt: string; // ISO-8601
 }
+export interface SurveyAssignmentID extends SurveyAssignment, ID { studentId: string; }
 
 export interface SurveyResponse extends SurveyAssignment {
   responseId: string;
   submittedAt: string; // ISO-8601
 }
+export interface SurveyResponseID extends SurveyResponse, ID { studentId: string; }
