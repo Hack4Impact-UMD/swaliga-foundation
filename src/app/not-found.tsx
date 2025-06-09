fc316c21837c81bf01a59744a8298bfaa01378e4
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-09 00:00:53 -0400

    loading, error, & not found pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,11 +5,11 @@ import styles from "./not-found.module.css";
 const NotFound: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1 className={`${styles.text} ${styles.bold}`}>404</h1>
+      <h1 className={styles.text}>404</h1>
       <p className={styles.text}>Page Not Found</p>
       <div className={styles.buttonContainer}>
         <Link href="/">
-          <button className={styles.button}>Go to Homepage</button>
+          <button className={styles.button}>Go to Home Page</button>
         </Link>
       </div>
     </div>
--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Loading.module.css";
-
-export default function Loading() {
-  return (
-    <div className={styles.container}>
-      <h1>Loading</h1>
-    </div>
-  );
-}
