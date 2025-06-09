5c683a813756c53148e6c3ed58810f461a0bc1c6
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-09 13:55:49 -0400

    removed Konva module

--- src/app/LoginPage.tsx
+++ src/app/LoginPage.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import React, { useEffect, useState, useCallback } from "react";
-import "konva/lib/shapes/Line";
-import { Stage, Layer, Line } from "react-konva/lib/ReactKonvaCore";
-import { Polygon, Dims } from "@/types/konva-types";
+import React, { useState } from "react";
 import styles from "./LoginPage.module.css";
-import { getPolygonBackground, getPolygonOverlay } from "./polygons";
 import GoogleButton from "react-google-button";
 import {
   loginUser,
@@ -24,9 +20,6 @@ enum LoginPageErrors {
 }
 
 export default function LoginPage() {
-  const [dims, setDims] = useState<Dims>({ width: 0, height: 0 });
-  const [polygonBackground, setPolygonBackground] = useState<Polygon[]>([]);
-  const [polygonOverlay, setPolygonOverlay] = useState<Polygon[]>([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVisibility, setPasswordVisibility] = useState(false);
@@ -35,40 +28,12 @@ export default function LoginPage() {
   const [isLogin, setIsLogin] = useState<boolean>(true);
   const router = useRouter();
 
-  useEffect(() => {
-    setAllPolygons(window.innerWidth, window.innerHeight);
-    window.addEventListener("resize", () => {
-      setAllPolygons(window.innerWidth, window.innerHeight);
-    });
-  }, []);
-
-  const setAllPolygons = (width: number, height: number) => {
-    setDims({ width, height });
-    setPolygonBackground(getPolygonBackground(width, height));
-    setPolygonOverlay(getPolygonOverlay(width, height));
-  };
-
-  // renders the polygons in the background of the Login Page from ./polygons.ts
-  const drawPolygon = useCallback(
-    (polygon: Polygon) => (
-      <Line
-        key={polygon.points.toString()}
-        points={polygon.points}
-        fill={polygon.fill}
-        closed
-        stroke="black"
-        strokeWidth={0}
-      />
-    ),
-    []
-  );
-
   // authenticates user
   const signInWithEmail = async () => {
     try {
       await loginUser(email, password);
     } catch (error: any) {
-      setError(error)
+      setError(error);
     }
   };
 
@@ -108,90 +73,80 @@ export default function LoginPage() {
   };
 
   return (
-      <div className={styles.container}>
-        <div className={styles.background}>
-          <Stage
-            className={styles.stage}
-            width={dims.width}
-            height={dims.height}
-          >
-            <Layer>
-              {polygonBackground.map(drawPolygon)}
-              {polygonOverlay.map(drawPolygon)}
-            </Layer>
-          </Stage>
-          <div className={styles.login}>
-            <h1 className={styles.login_title}>
-              {isLogin ? "Login" : "Sign Up"}
-            </h1>
-            <div className={styles.login_field_container}>
-              <input
-                type="email"
-                className={styles.login_field}
-                placeholder="email"
-                value={email}
-                onChange={(ev) => setEmail(ev.target.value)}
-              />
-            </div>
-            <div className={styles.login_field_container}>
-              <input
-                type={passwordVisibility ? "text" : "password"}
-                className={styles.login_field}
-                placeholder="password"
-                value={password}
-                onChange={(ev) => onPasswordChange(ev.target.value)}
-              />
-              <i
-                className={
-                  styles.icon +
-                  ` fas ${passwordVisibility ? "fa-eye" : "fa-eye-slash"}`
-                }
-                onClick={() => setPasswordVisibility(!passwordVisibility)}
-              />
-            </div>
-            {!isLogin && (
-              <div className={styles.login_field_container}>
-                <input
-                  type="password"
-                  className={styles.login_field}
-                  placeholder="confirm password"
-                  value={confirmPassword}
-                  onChange={(ev) => onConfirmPasswordChange(ev.target.value)}
-                />
-              </div>
-            )}
-            <div className={styles.forgot_password}>
-              <a href="/reset-password">Forgot password?</a>
-            </div>
-            <p className={styles.error}>{error}</p>
-            <button
-              className={styles.login_button}
-              onClick={isLogin ? signInWithEmail : createAccount}
-            >
-              Submit
-            </button>
-            <p className={styles.toggle_text}>
-              {isLogin ? "Don't" : "Already"} have an account?
-              <br />
-              Click{" "}
-              <a href="#" onClick={toggleLogin}>
-                here
-              </a>{" "}
-              to {isLogin ? "sign up" : "login"}
-            </p>
-            <div className={styles.google_button}>
-              <GoogleButton onClick={() => signInWithGoogle()} />
-            </div>
-            <Image
-              className={styles.logo}
-              onClick={() => router.push("https://swaligafoundation.org/")}
-              src="/swaliga-website-logo.png"
-              alt="swaliga-logo"
-              width={200}
-              height={200}
+    <div className={styles.container}>
+      <div className={styles.background}>
+        <div className={styles.login}>
+          <h1 className={styles.login_title}>
+            {isLogin ? "Login" : "Sign Up"}
+          </h1>
+          <div className={styles.login_field_container}>
+            <input
+              type="email"
+              className={styles.login_field}
+              placeholder="email"
+              value={email}
+              onChange={(ev) => setEmail(ev.target.value)}
             />
           </div>
+          <div className={styles.login_field_container}>
+            <input
+              type={passwordVisibility ? "text" : "password"}
+              className={styles.login_field}
+              placeholder="password"
+              value={password}
+              onChange={(ev) => onPasswordChange(ev.target.value)}
+            />
+            <i
+              className={
+                styles.icon +
+                ` fas ${passwordVisibility ? "fa-eye" : "fa-eye-slash"}`
+              }
+              onClick={() => setPasswordVisibility(!passwordVisibility)}
+            />
+          </div>
+          {!isLogin && (
+            <div className={styles.login_field_container}>
+              <input
+                type="password"
+                className={styles.login_field}
+                placeholder="confirm password"
+                value={confirmPassword}
+                onChange={(ev) => onConfirmPasswordChange(ev.target.value)}
+              />
+            </div>
+          )}
+          <div className={styles.forgot_password}>
+            <a href="/reset-password">Forgot password?</a>
+          </div>
+          <p className={styles.error}>{error}</p>
+          <button
+            className={styles.login_button}
+            onClick={isLogin ? signInWithEmail : createAccount}
+          >
+            Submit
+          </button>
+          <p className={styles.toggle_text}>
+            {isLogin ? "Don't" : "Already"} have an account?
+            <br />
+            Click{" "}
+            <a href="#" onClick={toggleLogin}>
+              here
+            </a>{" "}
+            to {isLogin ? "sign up" : "login"}
+          </p>
+          <div className={styles.google_button}>
+            <GoogleButton onClick={() => signInWithGoogle()} />
+          </div>
+          <Image
+            className={styles.logo}
+            onClick={() => router.push("https://swaligafoundation.org/")}
+            src="/swaliga-website-logo.png"
+            alt="swaliga-logo"
+            width={200}
+            height={200}
+          />
         </div>
       </div>
+    </div>
   );
 }
--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 import React, { useState, useEffect, useCallback, ChangeEvent } from "react";
-import "konva/lib/shapes/Line";
-import { Stage, Layer, Line } from "react-konva/lib/ReactKonvaCore";
-import { Polygon, Dims } from "@/types/konva-types";
 import styles from "./CreateAccountPage.module.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Timestamp } from "firebase/firestore";
@@ -30,160 +27,6 @@ export default function CreateAccountPage() {
     window.addEventListener("resize", updateDims);
     return () => window.removeEventListener("resize", updateDims);
   }, [updateDims]);
-
-  const generatePolygons = useCallback((dims: Dims) => {
-    const coords: Polygon[] = [
-      {
-        points: [
-          0,
-          0,
-          0,
-          dims.height / 30,
-          (50 / 100) * dims.width,
-          dims.height,
-          dims.width,
-          dims.height,
-          dims.width,
-          0.45 * dims.height,
-          dims.height / 60,
-          0,
-        ],
-        fill: "#D0D12A",
-      },
-      {
-        points: [
-          0,
-          dims.height / 60,
-          0,
-          dims.height,
-          (50 / 85) * dims.width,
-          dims.height,
-        ],
-        fill: "#295972",
-      },
-      {
-        points: [
-          dims.height / 60,
-          0,
-          dims.width,
-          0,
-          dims.width,
-          0.45 * dims.height,
-        ],
-        fill: "#295972",
-      },
-    ];
-    setPolygons(coords);
-  }, []);
-
-  const generatePolygonOverlay = useCallback((dims: Dims) => {
-    const coords: Polygon[] = [
-      {
-        points: [
-          dims.width / 2,
-          0.15 * dims.height,
-          0.875 * dims.width,
-          0.15 * dims.height,
-          0.875 * dims.width,
-          0.39375 * dims.height,
-          dims.width / 2,
-          0.225 * dims.height,
-        ],
-        fill: "#D0D12A",
-      },
-      {
-        points: [
-          dims.width / 2,
-          0.225 * dims.height,
-          0.875 * dims.width,
-          0.39375 * dims.height,
-          0.875 * dims.width,
-          0.85 * dims.height,
-          0.5 * dims.width,
-          0.85 * dims.height,
-        ],
-        fill: "#295972",
-      },
-    ];
-    const borderPolygon1 = {
-      points: [
-        dims.width / 2 + 670,
-        0.15 * dims.height - 55, // top-left corner
-        dims.width / 2 + 678,
-        0.15 * dims.height - 54, // top-right corner
-        dims.width / 2 + 678,
-        0.15 * dims.height + 557, // bottom-right corner
-        dims.width / 2 + 670,
-        0.15 * dims.height + 556, // bottom-left corner
-      ],
-      fill: "white",
-    };
-    const borderPolygon2 = {
-      points: [
-        dims.width / 2 - 591,
-        0.15 * dims.height - 55, // top-left corner
-        dims.width / 2 - 583,
-        0.15 * dims.height - 54, // top-right corner
-        dims.width / 2 - 583,
-        0.15 * dims.height + 557, // bottom-right corner
-        dims.width / 2 - 591,
-        0.15 * dims.height + 556, // bottom-left corner
-      ],
-      fill: "white",
-    };
-    const borderPolygon3 = {
-      points: [
-        dims.width / 2 - 583,
-        0.15 * dims.height + 549, // bottom-right corner
-        dims.width / 2 - 591,
-        0.15 * dims.height + 557, // bottom-left corner
-        dims.width / 2 + 678,
-        0.15 * dims.height + 557, // bottom-right corner
-        dims.width / 2 + 670,
-        0.15 * dims.height + 549, // bottom-left corner
-      ],
-      fill: "white",
-    };
-    const borderPolygon4 = {
-      points: [
-        dims.width / 2 - 591,
-        0.15 * dims.height - 57, // bottom-right corner
-        dims.width / 2 - 591,
-        0.15 * dims.height - 49, // bottom-left corner
-        dims.width / 2 + 678,
-        0.15 * dims.height - 49, // bottom-right corner
-        dims.width / 2 + 678,
-        0.15 * dims.height - 57, // bottom-left corner
-      ],
-      fill: "white",
-    };
-    const updatedCoords = [
-      ...coords,
-      borderPolygon1,
-      borderPolygon2,
-      borderPolygon3,
-      borderPolygon4,
-    ];
-    setPolygonOverlay(updatedCoords);
-  }, []);
-
-  useEffect(() => {
-    if (dims.width && dims.height) {
-      generatePolygons(dims);
-      generatePolygonOverlay(dims);
-    }
-  }, [dims, generatePolygons, generatePolygonOverlay]);
-
-  const drawPolygon = (polygon: Polygon) => (
-    <Line
-      key={polygon.points.toString()}
-      points={polygon.points}
-      fill={polygon.fill}
-      closed
-      stroke="black"
-      strokeWidth={0}
-    />
-  );
 
   interface RaceEthnicity {
     blackOrAfricanAmerican: boolean;
@@ -507,12 +350,6 @@ export default function CreateAccountPage() {
   return (
     <div className={styles.container}>
       <div className={styles.background}>
-        <Stage className={styles.stage} width={dims.width} height={dims.height}>
-          <Layer>
-            {polygons.map(drawPolygon)}
-            {polygonOverlay.map(drawPolygon)}
-          </Layer>
-        </Stage>
         <form className={styles.accountForm} onSubmit={handleSubmit}>
           {/* Fields for Name on School Record */}
           <div className={styles.formGroupRow}>
