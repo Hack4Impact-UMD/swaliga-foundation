f8a82c4252d9bec3e4b5ec2460f16fa4141034ac
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-10 11:54:29 -0400

    LoginPage UI

--- src/app/LoginPage.tsx
+++ src/app/LoginPage.tsx
@@ -8,32 +8,23 @@ import {
   signUpUser,
 } from "@/features/auth/authN/emailPasswordAuthN";
 import { signInWithGoogle } from "@/features/auth/authN/googleAuthN";
-import { useRouter } from "next/navigation";
-import Image from "next/image";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-enum LoginPageErrors {
-  ACCOUNT_CREATION_FAILED = "Failed to create account",
-  LOGIN_FAILED = "Failed to login",
-  PASSWORD_TOO_SHORT = "Password must be at least 6 characters",
-  PASSWORDS_DONT_MATCH = "Passwords do not match",
-}
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
+import { validatePassword } from "firebase/auth";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordVisibility, setPasswordVisibility] = useState(false);
+  const [isLoginMode, setIsLoginMode] = useState<boolean>(true);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
   const [confirmPassword, setConfirmPassword] = useState<string>("");
-  const [error, setError] = useState<LoginPageErrors | null>(null);
-  const [isLogin, setIsLogin] = useState<boolean>(true);
-  const router = useRouter();
+  const [error, setError] = useState<string>("");
 
   // authenticates user
   const signInWithEmail = async () => {
     try {
       await loginUser(email, password);
     } catch (error: any) {
-      setError(error);
+      setError(error.message);
     }
   };
 
@@ -47,105 +38,90 @@ export default function LoginPage() {
   };
 
   const toggleLogin = () => {
-    setIsLogin(!isLogin);
-    setError(null);
+    setIsLoginMode(!isLoginMode);
+    setError("");
     setEmail("");
     setPassword("");
     setConfirmPassword("");
   };
 
-  const onPasswordChange = (newPassword: string) => {
-    setPassword(newPassword);
-    if (!isLogin) {
-      setError(
-        newPassword.length < 6 ? LoginPageErrors.PASSWORD_TOO_SHORT : null
-      );
-    }
-  };
-
-  const onConfirmPasswordChange = (newConfirmPassword: string) => {
-    setConfirmPassword(newConfirmPassword);
-    setError(
-      newConfirmPassword === password
-        ? null
-        : LoginPageErrors.PASSWORDS_DONT_MATCH
-    );
-  };
+  const togglePasswordVisibility = () => setIsPasswordVisible((prev) => !prev);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.background}>
+    <div className={styles.page}>
+      <div className={styles.container}>
         <div className={styles.login}>
           <h1 className={styles.login_title}>
-            {isLogin ? "Login" : "Sign Up"}
+            {isLoginMode ? "Login" : "Sign Up"}
           </h1>
-          <div className={styles.login_field_container}>
-            <input
-              type="email"
-              className={styles.login_field}
-              placeholder="email"
-              value={email}
-              onChange={(ev) => setEmail(ev.target.value)}
-            />
-          </div>
-          <div className={styles.login_field_container}>
-            <input
-              type={passwordVisibility ? "text" : "password"}
-              className={styles.login_field}
-              placeholder="password"
-              value={password}
-              onChange={(ev) => onPasswordChange(ev.target.value)}
-            />
-            <i
-              className={
-                styles.icon +
-                ` fas ${passwordVisibility ? "fa-eye" : "fa-eye-slash"}`
-              }
-              onClick={() => setPasswordVisibility(!passwordVisibility)}
-            />
-          </div>
-          {!isLogin && (
+          <div className={styles.email_pw_container}>
             <div className={styles.login_field_container}>
               <input
-                type="password"
+                type="email"
                 className={styles.login_field}
-                placeholder="confirm password"
-                value={confirmPassword}
-                onChange={(ev) => onConfirmPasswordChange(ev.target.value)}
+                placeholder="Email"
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
               />
             </div>
-          )}
-          <div className={styles.forgot_password}>
-            <a href="/reset-password">Forgot password?</a>
+            <div className={styles.login_field_container}>
+              <input
+                type={isPasswordVisible ? "text" : "password"}
+                className={styles.login_field}
+                placeholder="Password"
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+              />
+              {isPasswordVisible ? (
+                <MdVisibility onClick={togglePasswordVisibility} />
+              ) : (
+                <MdVisibilityOff onClick={togglePasswordVisibility} />
+              )}
+            </div>
+            {!isLoginMode && (
+              <div className={styles.login_field_container}>
+                <input
+                  type="password"
+                  className={styles.login_field}
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(ev) => setConfirmPassword(ev.target.value)}
+                />
+              </div>
+            )}
+            <div className={styles.forgot_password}>
+              <a href="/reset-password">Forgot password?</a>
+            </div>
+            {error && <p className={styles.error}>{error}</p>}
+            <button
+              className={styles.login_button}
+              onClick={isLoginMode ? signInWithEmail : createAccount}
+            >
+              Submit
+            </button>
+            <p className={styles.toggle_text}>
+              {isLoginMode ? "Don't" : "Already"} have an account?
+              <br />
+              Click{" "}
+              <a href="#" onClick={toggleLogin}>
+                here
+              </a>{" "}
+              to {isLoginMode ? "sign up" : "login"}
+            </p>
           </div>
-          <p className={styles.error}>{error}</p>
-          <button
-            className={styles.login_button}
-            onClick={isLogin ? signInWithEmail : createAccount}
-          >
-            Submit
-          </button>
-          <p className={styles.toggle_text}>
-            {isLogin ? "Don't" : "Already"} have an account?
-            <br />
-            Click{" "}
-            <a href="#" onClick={toggleLogin}>
-              here
-            </a>{" "}
-            to {isLogin ? "sign up" : "login"}
-          </p>
-          <div className={styles.google_button}>
-            <GoogleButton onClick={() => signInWithGoogle()} />
+          <div className={styles.divider}>
+            <hr className={styles.divider_line} />
+            <p className={styles.divider_text}>OR</p>
+            <hr className={styles.divider_line} />
           </div>
-          <Image
-            className={styles.logo}
-            onClick={() => router.push("https://swaligafoundation.org/")}
-            src="/swaliga-website-logo.png"
-            alt="swaliga-logo"
-            width={200}
-            height={200}
-          />
+          <div className={styles.google_container}>
+            <GoogleButton
+              className={styles.google_container}
+              onClick={() => signInWithGoogle()}
+            />
+          </div>
         </div>
+        <div className={styles.inverted_bg} />
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styles from "./layout.module.css";
 import AuthProvider from "@/features/auth/AuthProvider";
 import Navbar from "@/components/Navbar";
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} ${styles.body}`}>
         <AuthProvider>
           <Navbar />
           {children as JSX.Element}
