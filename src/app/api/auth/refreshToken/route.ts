836de5d5806321427938ab889fe84348e8bb9075
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-22 12:27:25 -0400

    deleted outdated auth files

--- src/app/api/auth/refreshToken/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { auth } from "@/config/firebaseConfig";
-import { isRefreshTokenValid } from "@/features/auth/googleAuthorization";
-
-// refreshs the admin account's refresh token if it does not exist or has expired
-export async function GET(req: NextRequest): Promise<NextResponse> {
-  const valid = await isRefreshTokenValid();
-  return NextResponse.json(valid, { status: 200 });
-}
