f977cb941a5a0c963ddc3b4476b307795e359f61
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-21 23:20:44 -0400

    updated OAuth consent scopes

--- src/app/api/auth/consent/route.ts
+++ src/app/api/auth/consent/route.ts
@@ -3,7 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const redirectUri = encodeURIComponent('http://localhost:3000/api/auth/handler');
-  const scope = encodeURIComponent(['https://www.googleapis.com/auth/script.external_request', 'https://www.googleapis.com/auth/script.scriptapp'].join(' '));
+  const scopes: string[] = [
+    'https://www.googleapis.com/auth/forms',
+    'https://www.googleapis.com/auth/script.scriptapp'
+  ]
+  const scope = encodeURIComponent(scopes.join(' '));
   const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
   return NextResponse.redirect(authUrl);
 }
