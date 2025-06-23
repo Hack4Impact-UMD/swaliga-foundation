1dfcbc3640ef762ed2e82f580a799954fe684b84
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-23 14:39:02 -0400

    implemented OAuth 2.0 flow to get refresh & access tokens

--- src/app/api/auth/consent/route.ts
+++ src/app/api/auth/consent/route.ts
@@ -1,13 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const clientId = process.env.GOOGLE_CLIENT_ID;
   const redirectUri = encodeURIComponent('http://localhost:3000/api/auth/handler');
   const scopes: string[] = [
     'https://www.googleapis.com/auth/forms',
     'https://www.googleapis.com/auth/script.scriptapp'
   ]
   const scope = encodeURIComponent(scopes.join(' '));
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
+  const email = req.nextUrl.searchParams.get('email') || ''
+  const idToken = req.nextUrl.searchParams.get('idToken') || '';
+  if (!email || !idToken) {
+    return NextResponse.json('Invalid parameters', { status: 400 });
+  }
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&login_hint=${email}&access_type=offline&prompt=consent&state=${idToken}`;
   return NextResponse.redirect(authUrl);
 }
--- src/app/api/auth/handler/route.ts
+++ src/app/api/auth/handler/route.ts
@@ -1,7 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import { adminAuth } from "@/config/firebaseAdminConfig";
+import { DecodedIdToken } from "firebase-admin/auth";
 
 export async function GET(req: NextRequest) {
   const code = req.nextUrl.searchParams.get("code");
+  const idToken = req.nextUrl.searchParams.get("state");
+  if (!code || !idToken) {
+    return NextResponse.json('Invalid parameters', { status: 400 });
+  }
+
+  let decodedToken: DecodedIdToken;
+  try {
+    decodedToken = await adminAuth.verifyIdToken(idToken);
+  } catch (error) {
+    return NextResponse.json('Invalid ID token', { status: 401 });
+  }
+
   const response = await fetch('https://oauth2.googleapis.com/token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -13,6 +27,17 @@ export async function GET(req: NextRequest) {
       grant_type: 'authorization_code',
     }),
   });
+  if (!response.ok) {
+    return NextResponse.json('Error fetching token', { status: response.status });
+  }
   const tokenData = await response.json();
-  return NextResponse.json(tokenData, { status: 200 });
+  
+  await adminAuth.setCustomUserClaims(decodedToken.uid, {
+    role: decodedToken.role,
+    googleTokens: {
+      refreshToken: tokenData.refresh_token,
+      accessToken: tokenData.access_token,
+    }
+  })
+  return NextResponse.redirect('http://localhost:3000');
 }
