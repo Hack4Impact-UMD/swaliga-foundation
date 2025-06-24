7fd133341b229d6a193abe2435540bbf3927f66c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-06-24 15:11:38 -0400

    added ID Token verification to /api/auth/consent endpoint

--- src/app/api/auth/consent/route.ts
+++ src/app/api/auth/consent/route.ts
@@ -1,17 +1,24 @@
+import { adminAuth } from "@/config/firebaseAdminConfig";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const redirectUri = encodeURIComponent(`${process.env.NEXT_PUBLIC_DOMAIN}/api/auth/handler`);
-  const scopes: string[] = [
+  const scopes: string = encodeURIComponent([
     'https://www.googleapis.com/auth/forms',
     'https://www.googleapis.com/auth/script.scriptapp'
-  ]
-  const scope = encodeURIComponent(scopes.join(' '));
-  const email = req.nextUrl.searchParams.get('email') || ''
+  ].join(' '));
   const idToken = req.nextUrl.searchParams.get('idToken') || '';
-  if (!email || !idToken) {
-    return NextResponse.json('Invalid parameters', { status: 400 });
+  if (!idToken) {
+    return NextResponse.json('Invalid parameters', { status: 400, statusText: 'Bad Request' });
   }
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&login_hint=${email}&access_type=offline&prompt=consent&state=${idToken}`;
+
+  let decodedToken;
+  try {
+    decodedToken = await adminAuth.verifyIdToken(idToken);
+  } catch (error) {
+    return NextResponse.json('Unauthorized', { status: 401, statusText: 'Unauthorized' });
+  }
+
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=${scopes}&login_hint=${decodedToken.email}&access_type=offline&prompt=consent&state=${idToken}`;
   return NextResponse.redirect(authUrl);
 }
