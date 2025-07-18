b675b2f67fcae12e8eab51a6601aeeb70c03d32a
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-07-18 13:58:37 -0400

    refactored Modal component

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,24 +1,35 @@
-import Image from 'next/image';
-import styles from './Modal.module.css';
-import closeIcon from '@/../public/icons/close-icon.png';
+"use client";
+
+import Image from "next/image";
+import styles from "./Modal.module.css";
+import closeIcon from "@/../public/icons/close-icon.png";
+import { cloneElement, useState } from "react";
 
 interface ModalProps {
-  children: JSX.Element;
-  closeModal: () => void;
-  width: number;
-  height: number;
+  children: [JSX.Element, React.ReactNode];
 }
 
 export default function Modal(props: ModalProps) {
-  const { children, closeModal, width, height } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { children } = props;
+  const [trigger, content] = children;
+
+  const toggleModal = () => setIsOpen((prev) => !prev);
+
   return (
-    <div className={styles.modalBackground}>
-      <dialog className={styles.modal} open style={{ width, height }}>
-        <span className={styles.closeIcon} onClick={closeModal} >
-          <Image src={closeIcon} alt="Close Icon" />
-        </span>
-        {children}
-      </dialog>
-    </div>
+    <>
+      {cloneElement(trigger, { onClick: toggleModal })}
+      {isOpen && (
+        <div className={styles.modalBackground}>
+          <dialog className={styles.modal}>
+            <span className={styles.closeIcon} onClick={toggleModal}>
+              <Image src={closeIcon} alt="Close Icon" />
+            </span>
+            {content}
+          </dialog>
+        </div>
+      )}
+    </>
   );
 }
