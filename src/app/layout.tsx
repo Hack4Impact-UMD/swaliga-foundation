2e9823cfb000a9388c8730b803d4d2097499271c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-07-24 20:41:44 -0400

    added Suspense to layout.tsx to resolve build issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import styles from "./layout.module.css";
 import AuthProvider from "@/features/auth/AuthProvider";
 import Navbar from "@/components/ui/Navbar";
 import DataProvider from "@/data/hooks/DataProvider";
+import { Suspense } from "react";
+import LoadingPage from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,10 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${styles.body}`}>
-        <AuthProvider>
-          <Navbar />
-          <DataProvider>{children as JSX.Element}</DataProvider>
-        </AuthProvider>
+        <Suspense fallback={<LoadingPage />}>
+          <AuthProvider>
+            <Navbar />
+            <DataProvider>{children as JSX.Element}</DataProvider>
+          </AuthProvider>
+        </Suspense>
       </body>
     </html>
   );
