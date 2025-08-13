64d5d0e4ae90d458cb7badd7f0df1c477931b2ff
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-08-13 01:41:46 -0400

    added pages for STUDENT role to / route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,27 @@ import RoleBasedPage from "@/features/auth/RoleBasedPage";
 import LoginPage from "./LoginPage";
 import useAuth from "@/features/auth/useAuth";
 import SendVerificationEmailPage from "./SendVerificationEmailPage";
+import StudentPage from "./students/[studentId]/StudentPage";
+import CreateAccountPage from "./create-account/CreateAccountPage";
 
 export default function LoginPageWrapper() {
   const auth = useAuth();
-  if (auth.token && !auth.token?.claims.email_verified) {
-    return <SendVerificationEmailPage />;
-  }
+  const studentId = auth.token?.claims.studentId as string;
 
   return (
-    <RequireAuth allowedRoles={[]} allowUnauthenticated>
-      <RoleBasedPage rolePages={{}} unauthenticatedPage={<LoginPage />} />
+    <RequireAuth allowedRoles={["STUDENT"]} allowUnauthenticated allowNoRole>
+      <RoleBasedPage
+        rolePages={{
+          STUDENT: () =>
+            studentId ? (
+              <StudentPage studentId={studentId} />
+            ) : (
+              <CreateAccountPage />
+            ),
+        }}
+        unauthenticatedPage={<LoginPage />}
+        noRolePage={<SendVerificationEmailPage />}
+      />
     </RequireAuth>
   );
 }
