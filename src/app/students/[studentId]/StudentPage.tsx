ca03bb01a2f504e5732bc5f5ec8b90d1bdbc99f3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-08-16 23:33:57 -0400

    swapped out Accordions for Tables on StudentPage

--- src/app/students/[studentId]/StudentPage.tsx
+++ src/app/students/[studentId]/StudentPage.tsx
@@ -8,6 +8,7 @@ import {
   isPendingAssignmentID,
   PendingAssignmentID,
   SurveyResponseID,
+  SurveyResponseStudentIdID,
 } from "@/types/survey-types";
 import { getFullAddress, getFullName, Role, Student } from "@/types/user-types";
 import moment from "moment";
@@ -27,12 +28,15 @@ import useAssignments from "@/data/hooks/useAssignments";
 import {
   FaAddressBook,
   FaBirthdayCake,
+  FaEdit,
   FaEnvelope,
+  FaEye,
   FaIdBadge,
   FaPhone,
   FaVenusMars,
 } from "react-icons/fa";
 import { FaPeopleGroup } from "react-icons/fa6";
+import Table, { Column } from "@/components/ui/Table";
 
 interface StudentPageProps {
   studentId: string;
@@ -41,8 +45,6 @@ interface StudentPageProps {
 export default function StudentPage(props: StudentPageProps) {
   const { studentId } = props;
 
-  const [openPendingId, setOpenPendingId] = useState<string | null>(null);
-  const [openResponseId, setOpenResponseId] = useState<string | null>(null);
   const [error, setError] = useState<string>("");
 
   const role = useAuth().token?.claims.role as Role;
@@ -56,11 +58,11 @@ export default function StudentPage(props: StudentPageProps) {
   );
 
   const pendingAssignments: PendingAssignmentID[] = [];
-  const surveyResponses: SurveyResponseID[] = [];
+  const surveyResponses: SurveyResponseStudentIdID[] = [];
   assignments.forEach((assignment) =>
     isPendingAssignmentID(assignment)
       ? pendingAssignments.push(assignment)
-      : surveyResponses.push(assignment)
+      : surveyResponses.push(assignment as SurveyResponseStudentIdID)
   );
 
   const studentInfo = [
@@ -101,6 +103,44 @@ export default function StudentPage(props: StudentPageProps) {
     },
   ];
 
+  const pendingAssignmentsColumns: Column<PendingAssignmentID>[] = [
+    {
+      name: "Survey Name",
+      getValue: (assignment: PendingAssignmentID) =>
+        surveys.find((survey) => survey.id === assignment.surveyId)!.name,
+    },
+    {
+      name: "Assignment Date",
+      getValue: (assignment: PendingAssignmentID) =>
+        moment(assignment.assignedAt).format("MMM D, YYYY"),
+    },
+    {
+      name: "Respond",
+      getValue: (assignment: PendingAssignmentID) => (
+        <FaEdit className={styles.icon} size={20} />
+      ),
+    },
+  ];
+
+  const responsesColumns: Column<SurveyResponseStudentIdID>[] = [
+    {
+      name: "Survey Name",
+      getValue: (assignment: SurveyResponseStudentIdID) =>
+        surveys.find((survey) => survey.id === assignment.surveyId)!.name,
+    },
+    {
+      name: "Submission Date",
+      getValue: (assignment: SurveyResponseStudentIdID) =>
+        moment(assignment.submittedAt).format("MMM D, YYYY"),
+    },
+    {
+      name: "View Response",
+      getValue: (assignment: SurveyResponseStudentIdID) => (
+        <FaEye className={styles.icon} size={20} />
+      ),
+    },
+  ];
+
   return (
     <div className={styles.page}>
       <div className={styles.container}>
@@ -120,97 +160,27 @@ export default function StudentPage(props: StudentPageProps) {
         </div>
         <h2 className={styles.subheader}>Surveys</h2>
         <h3 className={styles.surveysSectionHeader}>Pending Assignments</h3>
-        <Accordion
-          className={styles.accordion}
-          type="multiple"
-          value={openPendingId ? [openPendingId] : []}
-        >
-          {pendingAssignments.map((assignment) => {
-            const survey = surveys.find(
-              (survey) => survey.id === assignment.surveyId
-            )!;
-            return (
-              <AccordionItem
-                className={styles.pendingRow}
-                value={assignment.id}
-                key={assignment.id}
-              >
-                <AccordionHeader className={styles.rowHeader}>
-                  <p>{survey.name}</p>
-                  <AccordionTrigger className={styles.rowTrigger}>
-                    {openPendingId === assignment.id ? (
-                      <GoTriangleUp
-                        size={30}
-                        onClick={() => setOpenPendingId(null)}
-                      />
-                    ) : (
-                      <GoTriangleDown
-                        size={30}
-                        onClick={() => setOpenPendingId(assignment.id)}
-                      />
-                    )}
-                  </AccordionTrigger>
-                </AccordionHeader>
-                <AccordionContent>
-                  <iframe
-                    className={styles.embeddedForm}
-                    src={`${survey.responderUri}?entry.${survey.idQuestionEntryNumber}=${student.id}`}
-                  />
-                </AccordionContent>
-              </AccordionItem>
-            );
-          })}
-        </Accordion>
+        <div className={styles.tableContainer}>
+          <Table
+            items={pendingAssignments}
+            columns={pendingAssignmentsColumns}
+            paginationOptions={{
+              itemsPerPageOptions: [5, 10, 20],
+              includeAllOption: true,
+            }}
+          />
+        </div>
         <h3 className={styles.surveysSectionHeader}>Past Responses</h3>
-        <Accordion
-          className={styles.accordion}
-          type="multiple"
-          value={openResponseId ? [openResponseId] : []}
-        >
-          {surveyResponses.map((assignment) => {
-            const survey = surveys.find(
-              (survey) => survey.id === assignment.surveyId
-            )!;
-            const isResponseEditable = true;
-            return (
-              <AccordionItem
-                className={styles.responseRow}
-                value={assignment.id}
-                key={assignment.id}
-              >
-                <AccordionHeader className={styles.rowHeader}>
-                  <p>{survey.name}</p>
-                  <p>
-                    {moment(assignment.submittedAt).format("M/D/YYYY HH:mm:ss")}
-                  </p>
-                  {isResponseEditable && (
-                    <AccordionTrigger className={styles.rowTrigger}>
-                      {openResponseId === assignment.id ? (
-                        <GoTriangleUp
-                          size={30}
-                          onClick={() => setOpenResponseId(null)}
-                        />
-                      ) : (
-                        <GoTriangleDown
-                          size={30}
-                          onClick={() => setOpenResponseId(assignment.id)}
-                        />
-                      )}
-                    </AccordionTrigger>
-                  )}
-                </AccordionHeader>
-                {isResponseEditable && (
-                  <AccordionContent>
-                    <iframe
-                      className={styles.embeddedForm}
-                      src={`${survey.responderUri}?edit2=${assignment.responseId}`}
-                    />
-                  </AccordionContent>
-                )}
-              </AccordionItem>
-            );
-          })}
-        </Accordion>
+        <div className={styles.tableContainer}>
+          <Table
+            items={surveyResponses}
+            columns={responsesColumns}
+            paginationOptions={{
+              itemsPerPageOptions: [5, 10, 20],
+              includeAllOption: true,
+            }}
+          />
+        </div>
       </div>
     </div>
   );
