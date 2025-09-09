52da71251238972c80f2732ec11e9af820aaa137
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-09-09 10:32:07 -0400

    added image domain for Google profile pics

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  reactStrictMode: false
 };
 
 export default nextConfig;
