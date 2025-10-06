3bf750f295a69da5807c2bf3a70089f1fda85a67
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-10-05 23:43:32 -0400

    removed server library from client

--- src/features/auth/authZ/googleAuthZ.ts
+++ src/features/auth/authZ/googleAuthZ.ts
@@ -1,30 +1,20 @@
 import { auth } from "@/config/firebaseConfig";
-import { OAuth2Client } from "google-auth-library";
 import { getFunctionsURL } from "@/config/utils";
 
 export function getOAuth2ConsentURL(): string {
   const user = auth.currentUser;
   const email = user?.email || undefined;
 
-  const oAuth2Client = new OAuth2Client({
-    clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-    redirectUri: getFunctionsURL("handleOAuth2Code")
-  })
+  const redirectUri = encodeURIComponent(getFunctionsURL("handleOAuth2Code"));
+  const scope = encodeURIComponent([
+    'https://www.googleapis.com/auth/script.external_request',
+    'https://www.googleapis.com/auth/script.scriptapp',
+    'https://www.googleapis.com/auth/forms',
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://mail.google.com/'
+  ].join(' '));
 
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    include_granted_scopes: true,
-    login_hint: email,
-    prompt: 'consent',
-    response_type: 'code',
-    scope: [
-      'https://www.googleapis.com/auth/script.external_request',
-      'https://www.googleapis.com/auth/script.scriptapp',
-      'https://www.googleapis.com/auth/forms',
-      'https://www.googleapis.com/auth/drive',
-      'https://www.googleapis.com/auth/spreadsheets',
-      'https://mail.google.com/'
-    ].join(' ')
-  })
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&login_hint=${email}&access_type=offline&prompt=consent&include_granted_scopes=true`;
   return authUrl;
 }
