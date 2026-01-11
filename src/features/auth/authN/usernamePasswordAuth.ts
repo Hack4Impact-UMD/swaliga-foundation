428edd255aa09c2c93221f6ef46ec04dd4dc1399
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-01-10 21:45:41 -0500

    create frontend username/password auth functions

---
+++ src/features/auth/authN/usernamePasswordAuth.ts
@@ -0,0 +1,25 @@
+import { auth } from "@/config/firebaseConfig";
+import { getFunctionsURL } from "@/config/utils";
+import { signInWithCustomToken } from "firebase/auth";
+
+export async function signUpWithUsernamePassword(username: string, password: string) {
+  await fetch(getFunctionsURL('signUpUsernamePassword'), {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      password
+    })
+  });
+}
+
+export async function loginWithUsernamePassword(username: string, password: string) {
+  const res = await fetch(getFunctionsURL("loginUsernamePassword"), {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      password
+    })
+  });
+  const { token } = await res.json();
+  await signInWithCustomToken(auth, token);
+}
