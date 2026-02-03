5eff5e8c474a693537bc2965e528b327fff7efc1
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-02-03 08:58:48 -0500

    create getAdminAccessToken Cloud Function

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -3,7 +3,7 @@ import { Collection } from './types/serverTypes';
 import { StudentCustomClaims, StudentDecodedIdTokenWithCustomClaims } from '@/types/auth-types';
 import { SurveyResponseStudentEmailID } from '@/types/survey-types';
 import { FieldValue, Transaction } from 'firebase-admin/firestore';
-import { onCall, onRequest } from "firebase-functions/v2/https";
+import { HttpsError, onCall, onRequest } from "firebase-functions/v2/https";
 import { getFunctionsURL } from '@/config/utils';
 import { Credentials, OAuth2Client, TokenPayload } from 'google-auth-library';
 import moment from 'moment';
@@ -223,3 +223,17 @@ export const loginWithUsernamePassword = onRequest(async (req, res): Promise<voi
   }
   res.status(400).json({ status: 'error', error: 'Invalid username or password' });
 })
+
+export const getAdminAccessToken = onCall(async (req) => {
+  if (!req.auth || req.auth.token.role !== 'ADMIN') {
+    throw new HttpsError('permission-denied', "You are not authorized to access this resource.");
+  }
+  const adminUser = await adminAuth.getUserByEmail(process.env.ADMIN_EMAIL || "");
+  const uid = adminUser.uid;
+  const credentials = (await adminDb.collection(Collection.GOOGLE_OAUTH2_TOKENS).doc(uid).get()).data() as Credentials;
+  return {
+    accessToken: credentials.access_token,
+    expiryDate: credentials.expiry_date,
+    scope: credentials.scope
+  };
+});
