c9bec88aaaf8318d3e08e3f3eb8f06f0b0727430
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-02-28 16:08:35 -0500

    add description of missing fields to IncompleteProfileMessage

--- src/features/profile/IncompleteProfileMessage.tsx
+++ src/features/profile/IncompleteProfileMessage.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import useProfile from "./useProfile";
 
@@ -12,24 +12,42 @@ export default function IncompleteProfileMessage() {
   const isProfileIncomplete =
     !student.joinedSwaligaDate ||
     !student.address ||
-    !student.address.addressLine1 ||
-    !student.address.city ||
-    !student.address.state ||
-    !student.address.country ||
-    !student.address.zipCode ||
     !student.school.gradYear ||
     !student.school.gpa ||
     !student.school.address ||
-    !student.school.address.addressLine1 ||
-    !student.school.address.city ||
-    !student.school.address.state ||
-    !student.school.address.country ||
-    !student.school.address.zipCode ||
     student.guardians.some((guardian) => !guardian.email || !guardian.phone);
 
   if (!isProfileIncomplete) {
     return <></>;
   }
 
-  return <p>bruh</p>
+  const missingFields: string[] = [];
+  if (!student.joinedSwaligaDate) {
+    missingFields.push("Joined Swaliga Date");
+  }
+  if (!student.address) {
+    missingFields.push("Home Address");
+  }
+  if (!student.school.gradYear) {
+    missingFields.push("Graduation Year");
+  }
+  if (!student.school.gpa) {
+    missingFields.push("GPA");
+  }
+  if (!student.school.address) {
+    missingFields.push("School Address");
+  }
+  if (!student.guardians.some((guardian) => !guardian.email)) {
+    missingFields.push("at least 1 Guardian Email");
+  }
+  if (!student.guardians.some((guardian) => !guardian.phone)) {
+    missingFields.push("at least 1 Guardian Phone Number");
+  }
+
+  return (
+    <p>
+      Please complete your profile when you are able to. You are currently
+      missing the following fields: {missingFields.join(", ")}
+    </p>
+  );
 }
