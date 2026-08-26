f2937471d65a4c9132e2f355d064dd6051800bf9
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-08-25 21:59:35 -0400

    Revert "omit `isArchived` field from EditAccountForm"
    This reverts commit 888e527166228847e5e559a36c626ebe3b81d042.

--- src/features/accountManagement/components/EditAccountForm.tsx
+++ src/features/accountManagement/components/EditAccountForm.tsx
@@ -450,7 +450,7 @@ export default function EditAccountForm(props: EditAccountFormProps) {
     try {
       if (!auth.user) throw new Error("No authenticated user found.");
       // @ts-expect-error - UID should not be updated when admin users are editing a student's account
-      const studentDTO: Omit<Student, "id" | "isArchived"> = {
+      const studentDTO: Omit<Student, "id"> = {
         name: {
           firstName,
           ...(middleName.trim() ? { middleName: middleName.trim() } : {}),
