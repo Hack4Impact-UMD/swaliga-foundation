7295c57c14d35ac1370f578a9686c15536018d12
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-09-05 13:37:15 -0400

    create basic program type

--- src/types/club-types.ts
+++ src/types/club-types.ts
@@ -7,3 +7,21 @@ const ClubSchema = z.object({
   address: AddressSchema,
 });
 export type Club = z.infer<typeof ClubSchema>;
+
+const AGE_GROUPS = ["Varsity", "Junior Varsity"] as const;
+const AgeGroupSchema = z.enum(AGE_GROUPS);
+type AgeGroup = z.infer<typeof AgeGroupSchema>;
+
+const ProgramSchema = z.object({
+  id: z.uuid(),
+  clubId: z.uuid(),
+  name: z.string().min(1),
+  ageGroup: AgeGroupSchema,
+  startDate: z.preprocess(z => ),
+  endDate: z.preprocess(z => ),
+  studentIds: z.array(z.number()),
+  teacherIds: z.array(z.number()),
+  staffIds: z.array(z.number()),
+});
+export type Program = z.infer<typeof ProgramSchema>;
+
