c77048e6894f7f9ca8c3aeeae6c1ca3adf6be527
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-09-10 00:03:25 -0400

    leave comment on time

--- src/config/tanstackQuery.ts
+++ src/config/tanstackQuery.ts
@@ -3,7 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5
+      staleTime: 1000 * 60 * 5 // 5 minutes
     }
   }
 });
